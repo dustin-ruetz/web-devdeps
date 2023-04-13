b29649ae60591bd23b7774674ed5078911e980db
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-04-12 22:21:35 -0400

    feat: setup Prettier, add format NPM scripts

---
+++ src/.prettierrc.ts
@@ -0,0 +1,17 @@
+export const prettierConfig = {
+	// https://prettier.io/docs/en/options.html
+	arrowParens: "always",
+	bracketSameLine: false,
+	bracketSpacing: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: "preserve",
+	quoteProps: "as-needed",
+	semi: true,
+	singleAttributePerLine: false,
+	singleQuote: false,
+	tabWidth: 2,
+	trailingComma: "all",
+	// Refer to .editorconfig file for the advantages of using tabs over spaces for indentation.
+	useTabs: true,
+};
