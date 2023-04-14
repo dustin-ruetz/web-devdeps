2d49267f39f9e2a6ed0455ad37cc8a66eb3ada60
Dustin Ruetz <26169612+dustin-ruetz@users.noreply.github.com>
2023-04-13 21:27:32 -0400

    Configure Prettier (#2)
    * feat: setup Prettier, add format NPM scripts
    
    * feat(scripts/postbuild): rewrite script with appendFilesInLibCJS/copyFiles/renameFilesInLibCJS functions
    
    * feat(package.json): manually bump version to 0.3.0
    
    * feat(package.json): manually bump version to 0.3.0
    
    * fix(.prettierignore): ignore Prettier configs (.cjs and .js files)
    
    * feat(.prettierrc.ts): add more config options, document choices with code comments
    
    * feat(package.json): improve the format NPM script
    
    - Add the "--config" option to avoid Prettier needing to resolve the config file location.
    - Modify the "ignore-path" option so that Prettier uses the compiled file in "lib/".
    
    * fix(package.json): move the postbuild NPM script back with the other 'build' scripts
    
    * feat(package.json): add postinstall NPM script

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,14 +1,59 @@
 import {appendFile, copyFile, readdir, rename} from "node:fs/promises";
 
-const postbuild = async () => {
-	// Copy the uncompiled non-TypeScript files from src/ to lib/
+// Originally I wanted to use async/await with the forEach() function, but this doesn't always
+// work the way that I expect it to. MDN suggests using `for of` as a succinct alternative.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#composition
+
+/**
+ * Append files in lib/cjs/ with a `module.exports` line in order to:
+ * 1. simplify the codebase by having everything written in ESModule syntax,
+ * 2. export both CommonJS- and ESModule-compatible code from their respective lib/ directories, and
+ * 3. use tsc for building/compiling without needing to install additional dependencies.
+ */
+const appendFilesInLibCJS = async () => {
+	const cjsFiles = await readdir("lib/cjs/");
+
+	for (const file of cjsFiles) {
+		switch (file) {
+			case ".prettierrc.cjs": {
+				await appendFile(
+					"lib/cjs/.prettierrc.cjs",
+					"module.exports = exports.prettierConfig;\n",
+				);
+			}
+		}
+	}
+};
+
+/** Copy the uncompiled non-TypeScript files from src/ to lib/ */
+const copyFiles = async () => {
 	const srcFiles = await readdir("src/");
 
+	const dotFiles = srcFiles.filter((file) => {
+		if (file[0] === "." && !file.includes(".ts")) {
+			return file;
+		}
+	});
 	const jsonFiles = srcFiles.filter((file) => file.includes(".json"));
 
-	const filesToCopy = [...jsonFiles];
-	filesToCopy.forEach(async (file) => {
+	const filesToCopy = [...dotFiles, ...jsonFiles];
+	for (const file of filesToCopy) {
 		await copyFile(`src/${file}`, `lib/${file}`);
-	});
+	}
+};
+
+/** Rename files in lib/cjs/ by changing their extensions from .js to .cjs */
+const renameFilesInLibCJS = async () => {
+	const cjsFiles = await readdir("lib/cjs/");
+	const cjsFileNames = cjsFiles.map((file) => file.replace(".js", ""));
+	for (const fileName of cjsFileNames) {
+		await rename(`lib/cjs/${fileName}.js`, `lib/cjs/${fileName}.cjs`);
+	}
+};
+
+const postbuild = async () => {
+	await renameFilesInLibCJS();
+	await appendFilesInLibCJS();
+	await copyFiles();
 };
 postbuild();
---
+++ src/.prettierrc.ts
@@ -0,0 +1,60 @@
+export const prettierConfig = {
+	// https://prettier.io/docs/en/options.html
+	arrowParens: "always",
+	bracketSameLine: false,
+	bracketSpacing: false,
+	/**
+	 * Excerpt from https://prettier.io/docs/en/options.html#embedded-language-formatting:
+	 * > When Prettier identifies cases where it looks like you've placed some code it knows how to format
+	 * > within a string in another file, like in a tagged template in JavaScript with a tag named `html`
+	 * > or in code blocks in Markdown, it will by default try to format that code.
+	 */
+	embeddedLanguageFormatting: "auto",
+	/**
+	 * Excerpt from https://prettier.io/blog/2018/11/07/1.15.0.html#whitespace-sensitive-formatting:
+	 * > Whitespace is significant in HTML inline elements.
+	 * > For this reason, we cannot safely format some HTML code since it may modify the displayed output in the browser.
+	 * > Instead of breaking your code or just doing nothing, we introduce _whitespace-sensitive formatting_, which:
+	 * > - follows the default CSS `display` value for every element to identify if the whitespace inside it is significant,
+	 * > - and wraps the tags in such a way as to avoid adding or removing significant whitespace.
+	 *
+	 * > We also allow magic comments (e.g., `<!-- display: block -->`) to tell Prettier
+	 * > how to format elements due to the fact that CSS display can be changed.
+	 */
+	htmlWhitespaceSensitivity: "css",
+	jsxSingleQuote: false,
+	/**
+	 * Excerpt from https://prettier.io/docs/en/options.html#print-width:
+	 * > Specify the line length that the printer will wrap on.
+	 * > **For readability we recommend against using more than 80 characters.**
+	 * > Prettier’s `printWidth` option [...] is not the hard upper allowed line length limit.
+	 * > It is a way to say to Prettier roughly how long you'd like lines to be.
+	 * > Prettier will make both shorter and longer lines, but generally strive to meet the specified `printWidth`.
+	 */
+	printWidth: 80,
+	/**
+	 * Don't wrap lines when formatting text in Markdown files since some services
+	 * use a linebreak-sensitive renderer, e.g. GitHub comments.
+	 */
+	proseWrap: "preserve",
+	quoteProps: "as-needed",
+	semi: true,
+	singleAttributePerLine: false,
+	/**
+	 * Prefer double quotes for strings because single quotes are used more often
+	 * within sentences for contractions and to indicate possession. Examples:
+	 * - Contraction: "It's a personal preference."
+	 * - Possession: "This is Dustin's codebase."
+	 *
+	 * Excerpt from https://prettier.io/docs/en/options.html#quotes:
+	 * > If the number of quotes outweighs the other quote, the quote which is less used will be used to format the string. Examples:
+	 * > - "I'm double quoted" will result in in "I'm double quoted".
+	 * > - "This \"example\" is single quoted" will result in 'This "example" is single quoted'.
+	 */
+	singleQuote: false,
+	tabWidth: 2,
+	/** Add trailing commas wherever possible to help reduce the number of changed lines when viewing Git diffs. */
+	trailingComma: "all",
+	/** Refer to .editorconfig file for the advantages of using tabs over spaces for indentation. */
+	useTabs: true,
+};
