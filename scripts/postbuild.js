8237a50244f8ff77c27b6971cc7cae552641ed94
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-04-19 17:00:12 -0400

    docs(scripts/postbuild): code comment for appendPrettierConfigFile

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -11,7 +11,7 @@ import {appendFile, copyFile, readdir, rename, rm} from "node:fs/promises";
 
 /**
  * Note that Prettier requires a CommonJS file using `module.exports` in its current v2 iteration, but if this changes
- * in v3 then it may be possible to remove all CommonJS-related code/compilation entirely from this repo.
+ * in v3 then it may be possible to remove all CommonJS-related code/compilation from this repository entirely.
  *
  * Excerpt from https://prettier.io/docs/en/configuration.html:
  * > You can configure Prettier via a `.prettierrc.cjs` file that exports an object using `module.exports`.
