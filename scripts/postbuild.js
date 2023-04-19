e0c058c80686ad1bb4858092b51a648a6761f320
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-04-18 22:17:45 -0400

    feat(scripts/postbuild): update how lib/ directory is handled - Rename all files in /lib/cjs with the .cjs file extension. - Rename all files in /lib/esm with the .mjs file extension. - Copy all compiled files from both /lib/cjs/ and /lib/esm to the lib/ directory. - Copy all uncompiled files from /src/ to the /lib/ directory. - Delete the /lib/cjs/ and /lib/esm/ directories.

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,4 +1,4 @@
-import {appendFile, copyFile, readdir, rename} from "node:fs/promises";
+import {appendFile, copyFile, readdir, rename, rm} from "node:fs/promises";
 
 // Originally I wanted to use async/await with the forEach() function, but this doesn't always
 // work the way that I expect it to. MDN suggests using `for of` as a succinct alternative.
@@ -22,11 +22,21 @@ const appendPrettierConfigFile = async () => {
 		"module.exports = exports.prettierConfig;\n",
 	);
 };
+
+const copyCompiledFilesToLib = async () => {
+	const cjsFiles = await readdir("lib/cjs/");
+	for (const file of cjsFiles) {
+		await copyFile(`lib/cjs/${file}`, `lib/${file}`);
+	}
+
+	const esmFiles = await readdir("lib/esm/");
+	for (const file of esmFiles) {
+		await copyFile(`lib/esm/${file}`, `lib/${file}`);
 	}
 };
 
 /** Copy the uncompiled non-TypeScript files from src/ to lib/ */
-const copyFiles = async () => {
+const copyUncompiledFilesToLib = async () => {
 	const srcFiles = await readdir("src/");
 
 	const dotFiles = srcFiles.filter((file) => {
@@ -42,6 +52,12 @@ const copyFiles = async () => {
 	}
 };
 
+/** Delete the lib/cjs/ and lib/esm/ directories. */
+const deleteDirectoriesInLib = async () => {
+	await rm("lib/cjs/", {force: true, recursive: true});
+	await rm("lib/esm/", {force: true, recursive: true});
+};
+
 /** Rename files in lib/cjs/ by changing their extensions from .js to .cjs */
 const renameFilesInLibCJS = async () => {
 	const cjsFiles = await readdir("lib/cjs/");
@@ -50,8 +66,22 @@ const renameFilesInLibCJS = async () => {
 		await rename(`lib/cjs/${fileName}.js`, `lib/cjs/${fileName}.cjs`);
 	}
 };
+/** Rename files in lib/esm/ by changing their extensions from .js to .mjs */
+const renameFilesInLibESM = async () => {
+	const esmFiles = await readdir("lib/esm/");
+	const esmFileNames = esmFiles.map((file) => file.replace(".js", ""));
+	for (const fileName of esmFileNames) {
+		await rename(`lib/esm/${fileName}.js`, `lib/esm/${fileName}.mjs`);
+	}
+};
 
 const postbuild = async () => {
+	// Make use of `Promise.all` to kick off multiple asynchronous operations simultaneously
+	// and wait for all of them to resolve before proceeding to the next operation.
+	await Promise.all([renameFilesInLibCJS(), renameFilesInLibESM()]);
 	await appendPrettierConfigFile();
+	await Promise.all([copyCompiledFilesToLib(), copyUncompiledFilesToLib()]);
+	// Delete lib/cjs/ and lib/esm/ now that their child files have been copied to the lib/ directory.
+	await deleteDirectoriesInLib();
 };
 postbuild();
