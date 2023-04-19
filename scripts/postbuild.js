0e5a981e2024c617e3de5a5ba857512ac4219f5d
Dustin Ruetz <26169612+dustin-ruetz@users.noreply.github.com>
2023-04-18 22:38:17 -0400

    Flatten `lib/` directory (#4)
    * feat: update paths in config files
    
    * feat(scripts/postbuild): rename from appendFilesInLibCJS to appendPrettierConfigFile
    
    * feat(scripts/postbuild): update how lib/ directory is handled
    - Rename all files in lib/cjs with the .cjs file extension.
    - Rename all files in lib/esm with the .mjs file extension.
    - Copy all compiled files from both lib/cjs/ and lib/esm to the lib/ directory.
    - Copy all uncompiled files from src/ to lib/.
    - Delete the lib/cjs/ and lib/esm/ directories.
    
    * feat(package.json): manually bump version to 0.5.0

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,37 +1,42 @@
-import {appendFile, copyFile, readdir, rename} from "node:fs/promises";
+import {appendFile, copyFile, readdir, rename, rm} from "node:fs/promises";
 
 // Originally I wanted to use async/await with the forEach() function, but this doesn't always
 // work the way that I expect it to. MDN suggests using `for of` as a succinct alternative.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#composition
 
-/**
- * Append files in lib/cjs/ with a `module.exports` line in order to:
- * 1. simplify the codebase by having everything written in ESModule syntax,
- * 2. export both CommonJS- and ESModule-compatible code from their respective lib/ directories, and
- * 3. use tsc for building/compiling without needing to install additional dependencies.
- */
-const appendFilesInLibCJS = async () => {
-	const cjsFiles = await readdir("lib/cjs/");
+// Append certain files in lib/cjs/ with a `module.exports` line in order to:
+// 1. simplify the codebase by keeping everything in src/ written in ESModule syntax,
+// 2. export both CommonJS- and ESModule-compatible code from the lib/ directory, and
+// 3. use tsc for building/compiling without needing to install additional dependencies.
 
+/**
+ * Note that Prettier requires a CommonJS file using `module.exports` in its current v2 iteration, but if this changes
+ * in v3 then it may be possible to remove all CommonJS-related code/compilation entirely from this repo.
+ *
+ * Excerpt from https://prettier.io/docs/en/configuration.html:
+ * > You can configure Prettier via a `.prettierrc.cjs` file that exports an object using `module.exports`.
+ */
+const appendPrettierConfigFile = async () => {
+	await appendFile(
+		"lib/cjs/.prettierrc.cjs",
+		"module.exports = exports.prettierConfig;\n",
+	);
+};
+
+const copyCompiledFilesToLib = async () => {
+	const cjsFiles = await readdir("lib/cjs/");
 	for (const file of cjsFiles) {
-		switch (file) {
-			// Note that Prettier requires a CommonJS file using `module.exports` in its current v2 iteration, but if this changes
-			// in v3 then it may be possible to remove all CommonJS-related code/compilation entirely from this repo.
-			// Excerpt from https://prettier.io/docs/en/configuration.html:
-			// > You can configure Prettier via a `.prettierrc.cjs` file that exports an object using `module.exports`.
-			case ".prettierrc.cjs": {
-				await appendFile(
-					"lib/cjs/.prettierrc.cjs",
-					"module.exports = exports.prettierConfig;\n",
-				);
-				break;
-			}
-		}
+		await copyFile(`lib/cjs/${file}`, `lib/${file}`);
+	}
+
+	const esmFiles = await readdir("lib/esm/");
+	for (const file of esmFiles) {
+		await copyFile(`lib/esm/${file}`, `lib/${file}`);
 	}
 };
 
 /** Copy the uncompiled non-TypeScript files from src/ to lib/ */
-const copyFiles = async () => {
+const copyUncompiledFilesToLib = async () => {
 	const srcFiles = await readdir("src/");
 
 	const dotFiles = srcFiles.filter((file) => {
@@ -47,6 +52,12 @@ const copyFiles = async () => {
 	}
 };
 
+/** Delete the lib/cjs/ and lib/esm/ directories. */
+const deleteDirectoriesInLib = async () => {
+	await rm("lib/cjs/", {force: true, recursive: true});
+	await rm("lib/esm/", {force: true, recursive: true});
+};
+
 /** Rename files in lib/cjs/ by changing their extensions from .js to .cjs */
 const renameFilesInLibCJS = async () => {
 	const cjsFiles = await readdir("lib/cjs/");
@@ -55,10 +66,22 @@ const renameFilesInLibCJS = async () => {
 		await rename(`lib/cjs/${fileName}.js`, `lib/cjs/${fileName}.cjs`);
 	}
 };
+/** Rename files in lib/esm/ by changing their extensions from .js to .mjs */
+const renameFilesInLibESM = async () => {
+	const esmFiles = await readdir("lib/esm/");
+	const esmFileNames = esmFiles.map((file) => file.replace(".js", ""));
+	for (const fileName of esmFileNames) {
+		await rename(`lib/esm/${fileName}.js`, `lib/esm/${fileName}.mjs`);
+	}
+};
 
 const postbuild = async () => {
-	await renameFilesInLibCJS();
-	await appendFilesInLibCJS();
-	await copyFiles();
+	// Make use of `Promise.all` to kick off multiple asynchronous operations simultaneously
+	// and wait for all of them to resolve before proceeding to the next operation.
+	await Promise.all([renameFilesInLibCJS(), renameFilesInLibESM()]);
+	await appendPrettierConfigFile();
+	await Promise.all([copyCompiledFilesToLib(), copyUncompiledFilesToLib()]);
+	// Delete lib/cjs/ and lib/esm/ now that their child files have been copied to the lib/ directory.
+	await deleteDirectoriesInLib();
 };
 postbuild();
