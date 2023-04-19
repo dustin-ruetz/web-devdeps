dd0d9fa0614129f9ed8ae122dee1f9ecd8cf4fd7
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-04-18 20:45:29 -0400

    docs(scripts/postbuild.js): add code comments describing Prettier CommonJS code

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -15,6 +15,10 @@ const appendFilesInLibCJS = async () => {
 
 	for (const file of cjsFiles) {
 		switch (file) {
+			// Note that Prettier requires a CommonJS file using `module.exports` in its current v2 iteration, but if this changes
+			// in v3 then it may be possible to remove all CommonJS-related code/compilation entirely from this repo.
+			// Excerpt from https://prettier.io/docs/en/configuration.html:
+			// > You can configure Prettier via a `.prettierrc.cjs` file that exports an object using `module.exports`.
 			case ".prettierrc.cjs": {
 				await appendFile(
 					"lib/cjs/.prettierrc.cjs",
