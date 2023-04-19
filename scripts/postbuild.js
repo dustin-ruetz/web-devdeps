63f0a9f0b899ecfc8ea8de742a6412cbf5c0d7eb
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-04-18 22:15:56 -0400

    feat(scripts/postbuild): rename from appendFilesInLibCJS to appendPrettierConfigFile

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -4,29 +4,24 @@ import {appendFile, copyFile, readdir, rename} from "node:fs/promises";
 // work the way that I expect it to. MDN suggests using `for of` as a succinct alternative.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#composition
 
-/**
- * Append files in lib/cjs/ with a `module.exports` line in order to:
- * 1. simplify the codebase by having everything written in ESModule syntax,
- * 2. export both CommonJS- and ESModule-compatible code from their respective lib/ directories, and
- * 3. use tsc for building/compiling without needing to install additional dependencies.
- */
-const appendFilesInLibCJS = async () => {
-	const cjsFiles = await readdir("lib/cjs/");
+// Append certain files in lib/cjs/ with a `module.exports` line in order to:
+// 1. simplify the codebase by keeping everything in src/ written in ESModule syntax,
+// 2. export both CommonJS- and ESModule-compatible code from the lib/ directory, and
+// 3. use tsc for building/compiling without needing to install additional dependencies.
 
-	for (const file of cjsFiles) {
-		switch (file) {
-			// Note that Prettier requires a CommonJS file using `module.exports` in its current v2 iteration, but if this changes
-			// in v3 then it may be possible to remove all CommonJS-related code/compilation entirely from this repo.
-			// Excerpt from https://prettier.io/docs/en/configuration.html:
-			// > You can configure Prettier via a `.prettierrc.cjs` file that exports an object using `module.exports`.
-			case ".prettierrc.cjs": {
-				await appendFile(
-					"lib/cjs/.prettierrc.cjs",
-					"module.exports = exports.prettierConfig;\n",
-				);
-				break;
-			}
-		}
+/**
+ * Note that Prettier requires a CommonJS file using `module.exports` in its current v2 iteration, but if this changes
+ * in v3 then it may be possible to remove all CommonJS-related code/compilation entirely from this repo.
+ *
+ * Excerpt from https://prettier.io/docs/en/configuration.html:
+ * > You can configure Prettier via a `.prettierrc.cjs` file that exports an object using `module.exports`.
+ */
+const appendPrettierConfigFile = async () => {
+	await appendFile(
+		"lib/cjs/.prettierrc.cjs",
+		"module.exports = exports.prettierConfig;\n",
+	);
+};
 	}
 };
 
@@ -57,8 +52,6 @@ const renameFilesInLibCJS = async () => {
 };
 
 const postbuild = async () => {
-	await renameFilesInLibCJS();
-	await appendFilesInLibCJS();
-	await copyFiles();
+	await appendPrettierConfigFile();
 };
 postbuild();
