82f147371a7a1907e97ac4e3b01d4af850df4329
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-08-08 21:44:36 -0400

    test(.jestrc.ts): specify 'collectCoverageFrom' option, update 'rootDir' and 'roots' options

--- src/.jestrc.ts
+++ src/.jestrc.ts
@@ -38,6 +38,12 @@ const isolatedModules = true;
 
 /** https://jestjs.io/docs/configuration */
 const jestConfig: Config = {
+	// Specify `collectCoverageFrom` to ensure that Jest collects test coverage for all TypeScript files.
+	// Excerpt from https://jestjs.io/docs/configuration#collectcoveragefrom-array:
+	// > An array of glob patterns indicating a set of files for which coverage information should be collected.
+	// > If a file matches the specified glob pattern, coverage information will be collected for it even if
+	// > no tests exist for this file and it's never required in the test suite.
+	collectCoverageFrom: ["**/*.ts"],
 	// https://stackoverflow.com/questions/69567201/coveragepathignorepatterns-ignore-files-with-specific-ending
 	coveragePathIgnorePatterns: ["/node_modules/", ...binaryFileExtensions.list],
 	coverageThreshold: {
@@ -52,16 +58,16 @@ const jestConfig: Config = {
 	// > We recommend placing the extensions most commonly used in your project on the left, so if you are
 	// > using TypeScript, you may want to consider moving "ts" and/or "tsx" to the beginning of the array.
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
-	// Specify both `rootDir` and `roots` so that Jest 1) finds all of the test files, and 2) collects test coverage for everything.
+	// Specify both `rootDir` and `roots` so that Jest finds all of the test files.
 	// Excerpt from https://jestjs.io/docs/configuration#rootdir-string:
 	// > The root directory that Jest should scan for tests and modules within.
 	// > Oftentimes, you'll want to set this to `"src"` or `"lib"`, corresponding to where in your repository the code is stored.
-	rootDir: "../src/",
+	rootDir: "../",
 	// Excerpt from https://jestjs.io/docs/configuration#roots-arraystring:
 	// > A list of paths to directories that Jest should use to search for files in.
-	// > By default, `roots` has a single entry `<rootDir>` but there are cases where you may want to have multiple roots within one project,
-	// > for example roots: `["<rootDir>/src/", "<rootDir>/tests/"]`.
-	roots: ["../scripts/", "../src/"],
+	// > By default, `roots` has a single entry `<rootDir>` but there are cases where you may want to have multiple roots
+	// > within one project, for example roots: `["<rootDir>/src/", "<rootDir>/tests/"]`.
+	roots: ["<rootDir>/scripts/", "<rootDir>/src/"],
 	// Mock out the Node.js core modules during setup to avoid having to repeatedly mock them in every test file
 	// where the module being tested relies on methods that are imported from Node.js core modules.
 	// Excerpt from https://jestjs.io/docs/configuration#setupfilesafterenv-array:
@@ -69,7 +75,7 @@ const jestConfig: Config = {
 	// > is executed. Since `setupFiles` executes before the test framework is installed in the environment, this script file presents you
 	// > the opportunity of running some code immediately after the test framework has been installed in the environment but before
 	// > the test code itself. In other words, `setupFilesAfterEnv` modules are meant for code which is repeating in each test file.
-	setupFilesAfterEnv: [`../lib/${setupFiles.mockNodeCoreModules}`],
+	setupFilesAfterEnv: [`<rootDir>/lib/${setupFiles.mockNodeCoreModules}`],
 	// Explicitly declare either `"node"|"jsdom"` as the testing environment for each extending repository that uses this Jest config.
 	// Excerpt from https://jestjs.io/docs/configuration#testenvironment-string:
 	// > The test environment that will be used for testing. The default environment in Jest is a Node.js environment.
@@ -78,14 +84,14 @@ const jestConfig: Config = {
 	transform: {
 		[transformFileExtensions.binary]:
 			// Set the path to the transformer relative to the Jest <rootDir>.
-			`../lib/${transformers.binaryFile}`,
-		[transformFileExtensions.javascript]: `../lib/${transformers.babelJest}`,
+			`<rootDir>/lib/${transformers.binaryFile}`,
+		[transformFileExtensions.javascript]: `<rootDir>/lib/${transformers.babelJest}`,
 		[transformFileExtensions.typescript]: [
 			transformers.tsJest,
 			{
 				isolatedModules,
 				// Set the `tsconfig` config option due to the custom location of the TypeScript configuration file.
-				tsconfig: "lib/tsconfig.esm.json",
+				tsconfig: "<rootDir>/lib/tsconfig.esm.json",
 			},
 		],
 	},
