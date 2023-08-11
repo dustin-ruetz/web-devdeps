bebcc839893a50e1c71b4b9443461f2b6b656b70
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-08-10 20:48:12 -0400

    feat(package.json): install exact version numbers for NPM packages, add packageJson.test.ts

---
+++ __tests__/packageJson.test.ts
@@ -0,0 +1,22 @@
+// JSON files can be imported using the tsconfig.json `resolveJsonModule` option.
+// Excerpt from https://www.typescriptlang.org/tsconfig#resolveJsonModule:
+// > Allows importing modules with a `.json` extension, which is a common practice in Node projects.
+// > This includes generating a type for the `import` based on the static JSON shape.
+import packageJson from "../package.json";
+
+test("the most important configuration options are correct", () => {
+	expect(packageJson.author).toMatch("Dustin Ruetz");
+	expect(packageJson.name).toMatch("dr-devdeps");
+	expect(packageJson.type).toMatch("module");
+});
+
+test("that NPM packages are installed with exact version numbers", () => {
+	const dependencyVersions = [
+		...Object.values(packageJson.dependencies).map((version) => version),
+		...Object.values(packageJson.devDependencies).map((version) => version),
+	];
+
+	dependencyVersions.forEach((dependency) =>
+		expect(dependency).not.toContain("^"),
+	);
+});
