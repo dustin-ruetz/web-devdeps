d01c2b86faf881ac7c0c37fa9c04b495551cd31e
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-08-10 20:36:25 -0400

    feat(scripts/utils/copyUncompiledFilesToLib): simplify how dotFiles and jsonFiles are filtered

--- scripts/utils/copyUncompiledFilesToLib.ts
+++ scripts/utils/copyUncompiledFilesToLib.ts
@@ -4,12 +4,10 @@ import {copyFile, readdir} from "node:fs/promises";
 export const copyUncompiledFilesToLib = async () => {
 	const srcFiles = await readdir("src/");
 
-	const dotFiles = srcFiles.filter((file) => {
-		if (file[0] === "." && !file.includes(".ts")) {
-			return file;
-		}
-	});
-	const jsonFiles = srcFiles.filter((file) => file.includes(".json"));
+	const dotFiles = srcFiles.filter(
+		(file) => file.startsWith(".") && !file.endsWith(".ts"),
+	);
+	const jsonFiles = srcFiles.filter((file) => file.endsWith(".json"));
 
 	const filesToCopy = [...dotFiles, ...jsonFiles];
 	for (const file of filesToCopy) {
