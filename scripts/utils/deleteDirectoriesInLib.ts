a42fc654cff6dc1e1166519fce17a815723d9b57
Dustin Ruetz <dtj.ruetz@gmail.com>
2023-08-13 21:59:19 -0400

    refactor(scripts/utils/deleteDirectoriesInLib): simplify code

--- scripts/utils/deleteDirectoriesInLib.ts
+++ scripts/utils/deleteDirectoriesInLib.ts
@@ -2,10 +2,8 @@ import {rm} from "node:fs/promises";
 
 /** Delete the lib/cjs/ and lib/esm/ directories. */
 export const deleteDirectoriesInLib = async () => {
-	const directoriesToDelete = ["lib/cjs/", "lib/esm/"];
-
 	await Promise.all(
-		directoriesToDelete.map((directory) =>
+		["lib/cjs/", "lib/esm/"].map((directory) =>
 			rm(directory, {force: true, recursive: true}),
 		),
 	);
