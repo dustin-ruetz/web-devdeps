9855491a275b7eb5247402c5bcb41ebde83d149b
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-01-02 00:01:25 -0500

    test(.eslintrc): throw errors if 'parserOptions' or 'rules' objects are undefined

--- src/__tests__/.eslintrc.test.ts
+++ src/__tests__/.eslintrc.test.ts
@@ -5,6 +5,18 @@ test("it exports a configuration object", () => {
 });
 
 test("the most important configuration options are correct", () => {
+	// The configuration's `parserOptions` and `rules` objects may be `undefined`,
+	// so throw an error immediately if this is the case.
+	const getErrorMessage = (keyName: "parserOptions" | "rules") =>
+		`eslintConfig.${keyName} should be an object; value is undefined.`;
+	if (typeof eslintConfig.parserOptions === "undefined") {
+		throw new Error(getErrorMessage("parserOptions"));
+	}
+	if (typeof eslintConfig.rules === "undefined") {
+		throw new Error(getErrorMessage("rules"));
+	}
+
+	// Continue with the assertions now that the config object's structure has been verified.
 	expect(eslintConfig.env).toStrictEqual({
 		browser: true,
 		es2024: true,
@@ -16,11 +28,12 @@ test("the most important configuration options are correct", () => {
 		"plugin:@typescript-eslint/recommended",
 	]);
 	expect(eslintConfig.parser).toMatch("@typescript-eslint/parser");
-	expect(eslintConfig.parserOptions?.sourceType).toMatch("module");
+	expect(eslintConfig.parserOptions.sourceType).toMatch("module");
 	expect(eslintConfig.plugins).toStrictEqual(["@typescript-eslint"]);
 	expect(eslintConfig.root).toBe(true);
-	expect(eslintConfig.rules?.["no-console"]).toMatch("warn");
-	expect(eslintConfig.rules?.["no-var"]).toMatch("error");
+	expect(eslintConfig.rules.camelcase).toMatch("error");
+	expect(eslintConfig.rules["no-console"]).toMatch("warn");
+	expect(eslintConfig.rules["no-var"]).toMatch("error");
 	expect(eslintConfig.overrides).toStrictEqual([
 		{
 			files: ["*.test.+(js|jsx|ts|tsx)"],
