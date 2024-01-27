df48758705cd33026d9129ab9ea82310a7b8b65b
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-01-26 19:17:04 -0500

    feat(scripts/postbuild): simplify script by consolidating it to one file and deleting utils/

--- scripts/__tests__/postbuild.test.ts
+++ scripts/__tests__/postbuild.test.ts
@@ -1,28 +1,56 @@
-import {appendESLintConfigFile} from "../utils/appendESLintConfigFile";
-import {copyCompiledFilesToLib} from "../utils/copyCompiledFilesToLib";
-import {copyUncompiledFilesToLib} from "../utils/copyUncompiledFilesToLib";
-import {deleteDirectoriesInLib} from "../utils/deleteDirectoriesInLib";
-import {renameFilesInLibCJS} from "../utils/renameFilesInLibCJS";
-import {renameFilesInLibESM} from "../utils/renameFilesInLibESM";
+import {appendFile, copyFile, readdir, rename} from "node:fs/promises";
 import {postbuild} from "../postbuild";
 
-// Mock out everything to ensure that the code inside these imported modules isn't actually called.
-jest.mock("../utils/appendESLintConfigFile");
-jest.mock("../utils/copyCompiledFilesToLib");
-jest.mock("../utils/copyUncompiledFilesToLib");
-jest.mock("../utils/deleteDirectoriesInLib");
-jest.mock("../utils/renameFilesInLibCJS");
-jest.mock("../utils/renameFilesInLibESM");
+jest.mock("node:fs/promises", () => ({
+	appendFile: jest.fn(),
+	copyFile: jest.fn(),
+	readdir: jest.fn(() => [
+		"__tests__",
+		".eslintignore",
+		".prettierignore",
+		"commitlint.config.ts",
+		"eslint.config.ts",
+		"jest.config.ts",
+		"jestTransformerBabelJest.ts",
+		"jestTransformerBinaryFile.ts",
+		"jestTransformerSVGFile.ts",
+		"lint-staged.config.ts",
+		"prettier.config.ts",
+	]),
+	rename: jest.fn(),
+}));
 
 test("it runs all of the necessary post-build file system operations", async () => {
 	await postbuild();
 
-	// List the file system operations in the same order that they're called
-	// in `postbuild`, and verify that each one is called only once.
-	expect(renameFilesInLibCJS).toHaveBeenCalledTimes(1);
-	expect(renameFilesInLibESM).toHaveBeenCalledTimes(1);
-	expect(appendESLintConfigFile).toHaveBeenCalledTimes(1);
-	expect(copyCompiledFilesToLib).toHaveBeenCalledTimes(1);
-	expect(copyUncompiledFilesToLib).toHaveBeenCalledTimes(1);
-	expect(deleteDirectoriesInLib).toHaveBeenCalledTimes(1);
+	// Verify that the ESLint config file was renamed (from the .js to .cjs file extension).
+	expect(rename).toHaveBeenCalledTimes(1);
+	expect(rename).toHaveBeenCalledWith(
+		"lib/eslint.config.js",
+		"lib/eslint.config.cjs",
+	);
+
+	// Verify that the ESLint config file was appended to make it work with CommonJS.
+	expect(appendFile).toHaveBeenCalledTimes(1);
+	expect(appendFile).toHaveBeenCalledWith(
+		"lib/eslint.config.cjs",
+		"module.exports = exports.eslintConfig;",
+	);
+
+	// Verify that `readdir` was called on the src/ directory.
+	expect(readdir).toHaveBeenCalledTimes(1);
+	expect(readdir).toHaveBeenCalledWith("src/");
+
+	// Verify that the uncompiled files were copied from src/ to lib/
+	expect(copyFile).toHaveBeenCalledTimes(2);
+	expect(copyFile).toHaveBeenNthCalledWith(
+		1,
+		"src/.eslintignore",
+		"lib/.eslintignore",
+	);
+	expect(copyFile).toHaveBeenNthCalledWith(
+		2,
+		"src/.prettierignore",
+		"lib/.prettierignore",
+	);
 });
--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,22 +1,33 @@
-import {appendESLintConfigFile} from "./utils/appendESLintConfigFile";
-import {copyCompiledFilesToLib} from "./utils/copyCompiledFilesToLib";
-import {copyUncompiledFilesToLib} from "./utils/copyUncompiledFilesToLib";
-import {deleteDirectoriesInLib} from "./utils/deleteDirectoriesInLib";
-import {renameFilesInLibCJS} from "./utils/renameFilesInLibCJS";
-import {renameFilesInLibESM} from "./utils/renameFilesInLibESM";
+import {appendFile, copyFile, readdir, rename} from "node:fs/promises";
 
 export const postbuild = async () => {
-	// Make use of `Promise.all` to kick off multiple asynchronous operations simultaneously
-	// and wait for all of them to resolve before proceeding to the next block of operations.
-	await Promise.all([renameFilesInLibCJS(), renameFilesInLibESM()]);
-	// Append certain files in lib/cjs/ with a `module.exports` line in order to:
-	// 1. simplify the codebase by keeping everything in src/ written in ESModule syntax,
-	// 2. export both CommonJS- and ESModule-compatible code from the lib/ directory, and
-	// 3. use tsc for building/compiling without needing to install additional dependencies.
-	await Promise.all([appendESLintConfigFile()]);
-	await Promise.all([copyCompiledFilesToLib(), copyUncompiledFilesToLib()]);
-	// Delete lib/cjs/ and lib/esm/ now that their child files have been copied to the lib/ directory.
-	await deleteDirectoriesInLib();
+	// Note that ESLint requires a CommonJS file using `module.exports` in its current v8 iteration, but this changes as of v9;
+	// at that point it should be possible to remove all CommonJS-related code/compilation from this repository entirely.
+	const eslintConfigPath = {
+		base: "lib/eslint.config",
+		cjs() {
+			return `${this.base}.cjs`;
+		},
+		js() {
+			return `${this.base}.js`;
+		},
+	} as const;
+	await rename(eslintConfigPath.js(), eslintConfigPath.cjs());
+	// Append lib/eslint.config.cjs with a `module.exports` line in order to:
+	// 1. Simplify the codebase by keeping everything in src/ written in ESModule syntax;
+	// 2. Export both CommonJS- and ESModule-compatible code from the lib/ directory; and
+	// 3. Use tsc for building/compiling without relying on additional dependencies.
+	await appendFile(
+		eslintConfigPath.cjs(),
+		"module.exports = exports.eslintConfig;",
+	);
+
+	// Copy the uncompiled non-TypeScript files from src/ to lib/
+	const srcFiles = await readdir("src/");
+	const dotFiles = srcFiles.filter((file) => file.startsWith("."));
+	await Promise.all(
+		dotFiles.map((dotfile) => copyFile(`src/${dotfile}`, `lib/${dotfile}`)),
+	);
 };
 
 // Refer to scripts/prebuild.ts file for why these lines are being excluded from the test coverage report.
