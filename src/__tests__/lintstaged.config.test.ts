39d03d759c8efd8d2b4909bf331fcef52228a58b
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-02-01 22:04:08 -0500

    feat(lint-staged.config.ts): switch 'test' command from string to function

--- src/__tests__/lintstaged.config.test.ts
+++ src/__tests__/lintstaged.config.test.ts
@@ -3,3 +3,12 @@ import lintstagedConfig from "../lint-staged.config.js";
 test("it exports a configuration object", () => {
 	expect(typeof lintstagedConfig).toMatch("object");
 });
+
+test("it runs the correct test command", () => {
+	expect(
+		// @ts-expect-error TODO - figure out how to type the configuration object/this function correctly.
+		lintstagedConfig["*.{js,jsx,json,ts,tsx}"](["src/lint-staged.config.ts"]),
+	).toMatch(
+		"npm run vitest -- related --run src/lint-staged.config.ts --coverage.enabled --coverage.include=src/lint-staged.config.ts",
+	);
+});
--- src/lint-staged.config.ts
+++ src/lint-staged.config.ts
@@ -3,19 +3,28 @@ import type {Config} from "lint-staged";
 /** https://github.com/lint-staged/lint-staged */
 const lintstagedConfig: Config = {
 	/**
-	 * Note 1: Run "format" and "lint" as their base scripts (i.e. _not_ "format:check" and "lint:check")
+	 * Note: Run "format" and "lint" as their base scripts (i.e. _not_ "format:check" and "lint:check")
 	 * because lint-staged will pass just the paths of the staged files as an argument to these commands.
-	 *
-	 * Note 2: Running "test" with the "--findRelatedTests" flag scopes the test run to just the staged files.
 	 */
 	// Check code formatting for all file types that Prettier supports.
 	"*": "npm run format -- --check --ignore-unknown",
 	"*.{js,jsx,ts,tsx}": "npm run lint",
 	// This repo has unit tests for JSON files, so include them in the glob pattern.
-	"*.{js,jsx,json,ts,tsx}":
-		// Note that the `--collectCoverageFrom=` flag produces a scoped test coverage report; this flag is intentionally
-		// placed at the end of the command so that it correctly receives the paths that are passed to it by lint-staged.
-		"npm run test:coverage -- --findRelatedTests --collectCoverageFrom=",
+	"*.{js,jsx,json,ts,tsx}": (relativePaths) => {
+		const filesToIncludeInCoverage = relativePaths.map(
+			// Use `--coverage.include=${relativePath}` to generate a scoped test coverage report.
+			(relativePath) => `--coverage.include=${relativePath}`,
+		);
+
+		const testCommand =
+			"npm run vitest --" +
+			// Excerpt from https://vitest.dev/guide/cli.html#vitest-related:
+			// > Run only tests that cover a list of source files. All files should be relative to root folder.
+			` related --run ${relativePaths.join(" ")}` +
+			` --coverage.enabled ${filesToIncludeInCoverage.join(" ")}`;
+
+		return testCommand;
+	},
 	// Disable typechecking because of obtuse errors.
 	// "*.{ts,tsx}": "npm run typecheck",
 } as const;
