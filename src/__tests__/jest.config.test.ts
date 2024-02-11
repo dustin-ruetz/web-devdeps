c7fbc3fb22b10d9c10ec3dc3e8df26ee992f4e3d
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-02-10 20:11:10 -0500

    feat(jest.config): make Jest configuration dynamically adapt depending on package usage

--- src/__tests__/jest.config.test.ts
+++ src/__tests__/jest.config.test.ts
@@ -1,32 +1,109 @@
-import jestConfig, {getTransformConfig} from "../jest.config.js";
+import {type MockedFunction, vi} from "vitest";
+import {dependsOn} from "../utils/dependsOn.js";
+import {getRepoMetadata} from "../utils/getRepoMetadata.js";
+import {makeJestConfig} from "../jest.config.js";
 
-test("it exports a configuration object and a `getTransformConfig` function", () => {
-	expect(typeof jestConfig).toEqual("object");
-	expect(typeof getTransformConfig).toEqual("function");
+vi.mock("../utils/dependsOn.js", () => ({
+	dependsOn: vi.fn(),
+}));
+/**
+ * Usage: Call `mockDependsOn.mockResolvedValue(false|true)` based on the array of dependencies that are passed in to
+ * the function to determine the value of the `testEnvironment` variable in the jest.config.ts file. For example:
+ * - Resolve to `true` to simulate `testEnvironment` as `jsdom` because frontend dependencies *are* installed.
+ * - Resolve to `false` to simulate `testEnvironment` as `node` because frontend dependencies *are not* installed.
+ * */
+const mockDependsOn = dependsOn as MockedFunction<typeof dependsOn>;
+
+vi.mock("../utils/getRepoMetadata.js", () => ({
+	// Initialize the mock to return an empty object to prevent the following error from being thrown in the test run:
+	// > TypeError: Cannot destructure property 'absoluteRootDir' of 'getRepoMetadata(...)' as it is undefined.
+	getRepoMetadata: vi.fn(() => ({})),
+}));
+const mockGetRepoMetadata = getRepoMetadata as MockedFunction<
+	typeof getRepoMetadata
+>;
+/**
+ * Mirror the actual implementation of `getRepoMetadata().dependencyPartialPath`
+ * here since its value is always equal to the same string constant.
+ */
+const actualGetRepoMetadata = () =>
+	({
+		dependencyPartialPath: "node_modules/dr-devdeps",
+	}) as const;
+
+afterEach(() => {
+	vi.clearAllMocks();
 });
 
-test("the most important configuration options are correct", () => {
-	expect(jestConfig.rootDir).toEqual("../../../");
-	expect(jestConfig.testEnvironment).toEqual("node");
-	expect(jestConfig.verbose).toBe(true);
-});
+describe("the most important configuration options are correct", () => {
+	test("for the parts of the config that are determined by conditional logic", async () => {
+		const hasFrontendDependencies = false;
+		mockDependsOn.mockResolvedValue(hasFrontendDependencies);
+		mockGetRepoMetadata.mockReturnValue({
+			absoluteRootDir: "/Users/username/repos/dr-devdeps",
+			dependencyPartialPath: actualGetRepoMetadata().dependencyPartialPath,
+			isDevDepsRepo: true,
+		});
 
-test("the `transform` configuration is correct", () => {
-	// Verify the values of the config object.
-	expect(jestConfig.transform).toStrictEqual({
-		"(.jpg|.png|.woff2)":
-			"<rootDir>/node_modules/dr-devdeps/lib/jestTransformerBinaryFile.js",
-		".(js|jsx)":
-			"<rootDir>/node_modules/dr-devdeps/lib/jestTransformerBabelJest.js",
-		".svg": "<rootDir>/node_modules/dr-devdeps/lib/jestTransformerSVGFile.js",
-		".(ts|tsx)": [
-			"ts-jest",
-			{
-				tsconfig: "<rootDir>/node_modules/dr-devdeps/tsconfig.test.json",
-				useESM: true,
-			},
-		],
+		const jestConfig = await makeJestConfig();
+
+		expect(mockDependsOn).toHaveBeenCalledWith(["pug", "react"]);
+		expect(typeof jestConfig).toEqual("object");
+		expect(jestConfig.verbose).toBe(true);
 	});
-	// Verify the return value of the `getTransformConfig()` function.
-	expect(jestConfig.transform).toStrictEqual(getTransformConfig());
+
+	test("when testing this dr-devdeps repo (which *does not* have frontend dependencies)", async () => {
+		const hasFrontendDependencies = false;
+		mockDependsOn.mockResolvedValue(hasFrontendDependencies);
+		mockGetRepoMetadata.mockReturnValue({
+			absoluteRootDir: "/Users/username/repos/dr-devdeps",
+			dependencyPartialPath: actualGetRepoMetadata().dependencyPartialPath,
+			isDevDepsRepo: true,
+		});
+
+		const jestConfig = await makeJestConfig();
+
+		expect(jestConfig.coveragePathIgnorePatterns).toEqual(
+			expect.arrayContaining([
+				"<rootDir>/node_modules/",
+				"<rootDir>/src/jest.config.ts",
+			]),
+		);
+		expect(jestConfig.rootDir).toEqual("/Users/username/repos/dr-devdeps");
+		expect(jestConfig.testEnvironment).toEqual("node");
+		expect(jestConfig.testPathIgnorePatterns).toContain(
+			"<rootDir>/src/jest.config.ts",
+		);
+		// Sample the transform config object to verify that the paths to the transformer files are correct.
+		expect(jestConfig.transform?.[".(js|jsx)"]).toEqual(
+			"<rootDir>/lib/jestTransformerBabelJest.js",
+		);
+	});
+
+	test("when testing a repo that has installed the dr-devdeps package (repo *does* have frontend dependencies)", async () => {
+		const hasFrontendDependencies = true;
+		mockDependsOn.mockResolvedValue(hasFrontendDependencies);
+		mockDependsOn.mockResolvedValue(true);
+		mockGetRepoMetadata.mockReturnValue({
+			absoluteRootDir: "/Users/username/repos/consuming-repo",
+			dependencyPartialPath: actualGetRepoMetadata().dependencyPartialPath,
+			isDevDepsRepo: false,
+		});
+
+		const jestConfig = await makeJestConfig();
+
+		expect(jestConfig.coveragePathIgnorePatterns).toContain(
+			"<rootDir>/node_modules/",
+		);
+		expect(jestConfig.coveragePathIgnorePatterns).not.toContain(
+			"<rootDir>/src/jest.config.ts",
+		);
+		expect(jestConfig.rootDir).toEqual("/Users/username/repos/consuming-repo");
+		expect(jestConfig.testEnvironment).toEqual("jsdom");
+		expect(jestConfig.testPathIgnorePatterns).toStrictEqual([]);
+		// Sample the transform config object to verify that the paths to the transformer files are correct.
+		expect(jestConfig.transform?.[".(js|jsx)"]).toEqual(
+			"<rootDir>/node_modules/dr-devdeps/lib/jestTransformerBabelJest.js",
+		);
+	});
 });
--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,119 +1,124 @@
 import type {Config} from "jest";
-
-/** Binary file extensions 1) to ignore in test coverage, and 2) to transform the imported values to filenames. */
-const binaryFileExtensions = {
-	list: [".jpg", ".png", ".woff2"],
-	getTransformRegExp() {
-		return `(${this.list.join("|")})`;
-	},
-} as const;
-
-/**
- * The purpose of the `dr-devdeps` package is to provide standardized configurations and dependencies for other repositories to consume.
- * `getTransform` depends on the `consuming-repo` using the following folder/file structure and updates the paths accordingly:
- *
- * ```
- * // Created using the Shinotatwu-DS.file-tree-generator extension for Visual Studio Code.
- * 📂 consuming-repo
- * ┣ 📂 node_modules
- * ┃ ┗ 📂 dr-devdeps
- * ┃ ┃ ┗ 📂 lib
- * ┃ ┃ ┃ ┗ 📄 jest.config.js
- * ┗ 📄 package.json
- * ┗ 📄 tsconfig.json
- * ```
- */
-export const getTransformConfig = (overrides?: {
-	isDevDepsJestConfig: boolean;
-}) => {
-	/** The base path to the files/node_modules used as Jest transformers. */
-	const basePath = overrides?.isDevDepsJestConfig
-		? // If running tests on this `dr-devdeps` repo, set the path relative to the Jest <rootDir>.
-			"<rootDir>"
-		: // If running tests on a `consuming-repo`, set the path relative to its `dr-devdeps` dependency.
-			"<rootDir>/node_modules/dr-devdeps";
-
-	const transformConfig: Config["transform"] = {
-		[binaryFileExtensions.getTransformRegExp()]: `${basePath}/lib/jestTransformerBinaryFile.js`,
-		".(js|jsx)": `${basePath}/lib/jestTransformerBabelJest.js`,
-		".svg": `${basePath}/lib/jestTransformerSVGFile.js`,
-		".(ts|tsx)": [
-			"ts-jest",
-			{
-				// Set the `tsconfig` config option due to the need for a test-specific TypeScript configuration file.
-				tsconfig: `${basePath}/tsconfig.test.json`,
-				useESM: true,
-			},
-		],
-	} as const;
-
-	return transformConfig;
-};
+import {dependsOn} from "./utils/dependsOn.js";
+import {getRepoMetadata} from "./utils/getRepoMetadata.js";
 
 /** https://jestjs.io/docs/configuration */
-const jestConfig: Config = {
-	// Specify `collectCoverageFrom` to ensure that Jest collects test coverage for all JavaScript and TypeScript files.
-	// Excerpt from https://jestjs.io/docs/configuration#collectcoveragefrom-array:
-	// > An array of glob patterns indicating a set of files for which coverage information should be collected.
-	// > If a file matches the specified glob pattern, coverage information will be collected for it even if
-	// > no tests exist for this file and it's never required in the test suite.
-	collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}"],
-	// https://stackoverflow.com/questions/69567201/coveragepathignorepatterns-ignore-files-with-specific-ending
-	coveragePathIgnorePatterns: [
-		"<rootDir>/lib/",
-		"<rootDir>/node_modules/",
-		"<rootDir>/www/",
-		...binaryFileExtensions.list,
-		".svg",
-	],
-	// Specify `coverageReporters` to prevent the default reporters from writing a coverage/ directory to disk.
-	coverageReporters: ["text", "text-summary"],
-	coverageThreshold: {
-		global: {
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
+export const makeJestConfig = async (): Promise<Config> => {
+	/** Set the test environment based on whether or not the repo has any frontend dependencies. */
+	const testEnvironment = (await dependsOn(["pug", "react"]))
+		? "jsdom"
+		: "node";
+
+	const {absoluteRootDir, dependencyPartialPath, isDevDepsRepo} =
+		getRepoMetadata();
+
+	/** Binary file extensions 1) to ignore in test coverage, and 2) to transform the imported values to filenames. */
+	const binaryFileExtensions = {
+		list: [".jpg", ".png", ".woff2"],
+		makeTransformRegExp() {
+			return `(${this.list.join("|")})`;
 		},
-	},
-	// Excerpt from https://jestjs.io/docs/configuration#extensionstotreatasesm-arraystring:
-	// > Jest will run `.mjs` and `.js` files with nearest `package.json`'s `type` field set to `module` as ECMAScript Modules.
-	// > If you have any other files that should run with native ESM, you need to specify their file extension here.
-	extensionsToTreatAsEsm: [".jsx", ".ts", ".tsx"],
-	// Excerpt from https://jestjs.io/docs/configuration#modulefileextensions-arraystring:
-	// > We recommend placing the extensions most commonly used in your project on the left, so if you are
-	// > using TypeScript, you may want to consider moving "ts" and/or "tsx" to the beginning of the array.
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
-	// TypeScript knows how to map imported files with the ".js" extension to their ".ts" counterparts. Jest/ts-jest
-	// doesn't handle this automatically, so specify `moduleNameMapper` to handle `.js` and `.jsx` file extensions.
-	// Specify the `moduleNameMapper` to handle the fact that tests import ".js" file extensions.
-	// Excerpt from https://jestjs.io/docs/configuration#modulenamemapper-objectstring-string--arraystring:
-	// > A map from regular expressions to module names or to arrays of module names that
-	// > allow to stub out resources, like images or styles with a single module.
-	moduleNameMapper: {
-		// https://github.com/kulshekhar/ts-jest/issues/1057#issuecomment-1482644543:
-		"^(\\.\\.?\\/.+)\\.jsx?$": "$1",
-	},
-	// Specify the `rootDir` so that Jest finds all of the test files located in various directories.
-	// (i.e. <rootDir>/__tests__/, <rootDir>/scripts/ and <rootDir>/src/)
-	//
-	// Excerpt from https://jestjs.io/docs/configuration#rootdir-string:
-	// > The root directory that Jest should scan for tests and modules within.
-	// > Oftentimes, you'll want to set this to `"src"` or `"lib"`, corresponding to where in your repository the code is stored.
-	//
-	// This path traverses three levels upwards to get to the root directory of the `consuming-repo`.
-	// (i.e. starting from lib/, move upwards to dr-devdeps/ ^ node_modules/ ^ consuming-repo/)
-	rootDir: "../../../",
-	// Explicitly declare one of `"node"|"jsdom"` as the testing environment for each repository that uses this Jest config.
-	// Excerpt from https://jestjs.io/docs/configuration#testenvironment-string:
-	// > The test environment that will be used for testing. The default environment in Jest is a Node.js environment.
-	// > If you are building a web app, you can use a browser-like environment through `jsdom` instead.
-	testEnvironment: "node",
-	transform: getTransformConfig(),
-	// Don't transform anything in node_modules/, and don't transform .json files to prevent the following ts-jest warning:
-	// > `ts-jest[ts-jest-transformer] (WARN) Got a unknown file type to compile (file: *.json).`
-	transformIgnorePatterns: ["<rootDir>/node_modules/", ".json"],
-	verbose: true,
-} as const;
+	} as const;
+
+	/**
+	 * Folder/file patterns to ignore for this dr-devdeps repo because the unit tests are using `vi`
+	 * (instead of `jest`) for mocking and `v8` (instead of `istanbul`) as the coverage provider.
+	 */
+	const ignorePatterns = isDevDepsRepo
+		? ([
+				"<rootDir>/lib/",
+				"<rootDir>/scripts/",
+				"<rootDir>/src/utils/",
+				"<rootDir>/src/jest.config.ts",
+				"<rootDir>/src/__tests__/jest.config.test.ts",
+			] as const)
+		: ([] as const);
+
+	const makeTransformConfig = () => {
+		/** The base path to the files/packages used as Jest transformers. */
+		const basePath = isDevDepsRepo
+			? // If running tests on this `dr-devdeps` repo, set the path relative to the Jest <rootDir>.
+				"<rootDir>"
+			: // If running tests on a `consuming-repo`, set the path relative to its `dr-devdeps` dependency.
+				`<rootDir>/${dependencyPartialPath}`;
+
+		const transformConfig: Config["transform"] = {
+			[binaryFileExtensions.makeTransformRegExp()]: `${basePath}/lib/jestTransformerBinaryFile.js`,
+			".(js|jsx)": `${basePath}/lib/jestTransformerBabelJest.js`,
+			".svg": `${basePath}/lib/jestTransformerSVGFile.js`,
+			".(ts|tsx)": [
+				"ts-jest",
+				{
+					// Set the `tsconfig` config option due to ts-jest needing a test-specific TypeScript configuration file.
+					tsconfig: `${basePath}/tsconfig.test.json`,
+					useESM: true,
+				},
+			],
+		} as const;
+
+		return transformConfig;
+	};
+
+	return {
+		// Specify `collectCoverageFrom` to ensure that Jest collects test coverage for all JavaScript and TypeScript files.
+		// Excerpt from https://jestjs.io/docs/configuration#collectcoveragefrom-array:
+		// > An array of glob patterns indicating a set of files for which coverage information should be collected.
+		// > If a file matches the specified glob pattern, coverage information will be collected for it even if
+		// > no tests exist for this file and it's never required in the test suite.
+		collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}"],
+		// https://stackoverflow.com/questions/69567201/coveragepathignorepatterns-ignore-files-with-specific-ending
+		coveragePathIgnorePatterns: [
+			"<rootDir>/node_modules/",
+			"<rootDir>/www/",
+			...binaryFileExtensions.list,
+			".svg",
+			...ignorePatterns,
+		],
+		// Specify `coverageReporters` to prevent the default reporters from writing a coverage/ directory to disk.
+		coverageReporters: ["text", "text-summary"],
+		coverageThreshold: {
+			global: {
+				branches: 100,
+				functions: 100,
+				lines: 100,
+				statements: 100,
+			},
+		},
+		// Excerpt from https://jestjs.io/docs/configuration#extensionstotreatasesm-arraystring:
+		// > Jest will run `.mjs` and `.js` files with nearest `package.json`'s `type` field set to `module` as ECMAScript Modules.
+		// > If you have any other files that should run with native ESM, you need to specify their file extension here.
+		extensionsToTreatAsEsm: [".jsx", ".ts", ".tsx"],
+		// Excerpt from https://jestjs.io/docs/configuration#modulefileextensions-arraystring:
+		// > We recommend placing the extensions most commonly used in your project on the left, so if you are
+		// > using TypeScript, you may want to consider moving "ts" and/or "tsx" to the beginning of the array.
+		moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+		// TypeScript knows how to map imported files with the ".js" extension to their ".ts" counterparts. Jest/ts-jest
+		// doesn't handle this automatically, so specify `moduleNameMapper` to handle `.js` and `.jsx` file extensions.
+		// Specify the `moduleNameMapper` to handle the fact that tests import ".js" file extensions.
+		// Excerpt from https://jestjs.io/docs/configuration#modulenamemapper-objectstring-string--arraystring:
+		// > A map from regular expressions to module names or to arrays of module names that
+		// > allow to stub out resources, like images or styles with a single module.
+		moduleNameMapper: {
+			// https://github.com/kulshekhar/ts-jest/issues/1057#issuecomment-1482644543:
+			"^(\\.\\.?\\/.+)\\.jsx?$": "$1",
+		},
+		// Excerpt from https://jestjs.io/docs/configuration#rootdir-string:
+		// > The root directory that Jest should scan for tests and modules within.
+		// > Oftentimes, you'll want to set this to `"src"` or `"lib"`, corresponding to where in your repository the code is stored.
+		rootDir: absoluteRootDir,
+		// Excerpt from https://jestjs.io/docs/configuration#testenvironment-string:
+		// > The test environment that will be used for testing. The default environment in Jest is a Node.js environment.
+		// > If you are building a web app, you can use a browser-like environment through `jsdom` instead.
+		testEnvironment,
+		testPathIgnorePatterns: [...ignorePatterns],
+		transform: makeTransformConfig(),
+		// Don't transform anything in node_modules/, and don't transform .json files to prevent the following ts-jest warning:
+		// > `ts-jest[ts-jest-transformer] (WARN) Got a unknown file type to compile (file: *.json).`
+		transformIgnorePatterns: ["<rootDir>/node_modules/", ".json"],
+		verbose: true,
+	} as const;
+};
+
+const jestConfig = await makeJestConfig();
 
 export default jestConfig;
