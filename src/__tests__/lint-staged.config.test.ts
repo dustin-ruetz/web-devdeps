60905807e698bba479e46e335312bd03dda6ede1
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-02-11 21:31:29 -0500

    feat(lint-staged.config): support both Jest and Vitest as test runners for pre-commit Git hook

--- src/__tests__/lint-staged.config.test.ts
+++ src/__tests__/lint-staged.config.test.ts
@@ -5,17 +5,40 @@ test("it exports a configuration object", () => {
 	expect(typeof lintstagedConfig).toEqual("object");
 });
 
-test("it runs the correct commands", () => {
+test("it runs the correct commands for Prettier and ESLint", () => {
 	expect(lintstagedConfig["*" as keyof Config]).toEqual(
 		"npm run format -- --ignore-unknown --write",
 	);
 	expect(lintstagedConfig["*.{js,jsx,ts,tsx}" as keyof Config]).toEqual(
 		"npm run lint -- --fix",
 	);
-	expect(
-		// @ts-expect-error TODO - figure out how to type the function on this configuration object correctly.
-		lintstagedConfig["*.{js,jsx,json,ts,tsx}"](["src/lint-staged.config.ts"]),
-	).toEqual(
-		"npm run vitest -- related --run src/lint-staged.config.ts --coverage.enabled --coverage.include=src/lint-staged.config.ts",
-	);
+});
+
+describe("it runs the correct commands for unit testing when the TEST_RUNNER environment variable is equal to", () => {
+	/* eslint-disable dot-notation */
+	const relativePaths = ["src/lint-staged.config.ts"] as const;
+	const [relativePath] = relativePaths;
+
+	test("jest", () => {
+		process.env["TEST_RUNNER"] = "jest";
+
+		expect(
+			// @ts-expect-error TODO: Figure out how to type the function on this configuration object correctly.
+			lintstagedConfig["*.{js,jsx,json,ts,tsx}"](relativePaths),
+		).toEqual(
+			`npm run jest:coverage -- --findRelatedTests --collectCoverageFrom= ${relativePath}`,
+		);
+	});
+
+	test("vitest", () => {
+		process.env["TEST_RUNNER"] = "vitest";
+
+		expect(
+			// @ts-expect-error TODO
+			lintstagedConfig["*.{js,jsx,json,ts,tsx}"](relativePaths),
+		).toEqual(
+			`npm run vitest -- related --run ${relativePath} --coverage.enabled --coverage.include=${relativePath}`,
+		);
+	});
+	/* eslint-enable dot-notation */
 });
--- src/lint-staged.config.ts
+++ src/lint-staged.config.ts
@@ -11,19 +11,48 @@ const lintstagedConfig: Config = {
 	"*.{js,jsx,ts,tsx}": "npm run lint -- --fix",
 	// This repo has unit tests for JSON files, so include them in the glob pattern.
 	"*.{js,jsx,json,ts,tsx}": (relativePaths) => {
-		const filesToIncludeInCoverage = relativePaths.map(
-			// Use `--coverage.include=${relativePath}` to generate a scoped test coverage report.
-			(relativePath) => `--coverage.include=${relativePath}`,
-		);
+		const testRunners = {
+			jest: "jest",
+			vitest: "vitest",
+		} as const;
+		const {TEST_RUNNER} = process.env;
 
-		const testCommand =
-			"npm run vitest --" +
-			// Excerpt from https://vitest.dev/guide/cli.html#vitest-related:
-			// > Run only tests that cover a list of source files. All files should be relative to root folder.
-			` related --run ${relativePaths.join(" ")}` +
-			` --coverage.enabled ${filesToIncludeInCoverage.join(" ")}`;
+		switch (TEST_RUNNER) {
+			case testRunners.jest:
+			case typeof "undefined":
+			default: {
+				const jestCommand =
+					"npm run jest:coverage --" +
+					// Excerpt from https://jestjs.io/docs/cli#--findrelatedtests-spaceseparatedlistofsourcefiles:
+					// > Find and run the tests that cover a space-separated list of source files that were passed in as arguments.
+					// > Useful for pre-commit hook integration to run the minimal amount of tests necessary.
+					" --findRelatedTests" +
+					// Excerpt from https://jestjs.io/docs/cli#--collectcoveragefromglob:
+					// > A glob pattern relative to `rootDir` matching the files that coverage info needs to be collected from.
+					" --collectCoverageFrom=" +
+					// Important: Add a space between `--collectCoverageFrom=` and the relative paths to properly scope the coverage report.
+					` ${relativePaths.join(" ")}`;
 
-		return testCommand;
+				return jestCommand;
+			}
+			case testRunners.vitest: {
+				const filesToIncludeInCoverage = relativePaths.map(
+					// Use `--coverage.include` to generate a scoped test coverage report.
+					(relativePath) => `--coverage.include=${relativePath}`,
+				);
+
+				const vitestCommand =
+					"npm run vitest --" +
+					// Excerpt from https://vitest.dev/guide/cli.html#vitest-related:
+					// > Run only tests that cover a list of source files. All files should be relative to root folder.
+					` related --run ${relativePaths.join(" ")}` +
+					// > Excerpt from https://vitest.dev/config/#coverage-include:
+					// > List of files included in coverage as glob patterns.
+					` --coverage.enabled ${filesToIncludeInCoverage.join(" ")}`;
+
+				return vitestCommand;
+			}
+		}
 	},
 	// Disable typechecking because of obtuse errors.
 	// "*.{ts,tsx}": "npm run typecheck",
