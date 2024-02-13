c45eb06ae67b5013685e1446fb151b44821c5234
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-02-12 21:01:32 -0500

    feat(jestTransformerBinaryFile): specify 'node:path' as import, move code comment

--- src/jestTransformerBinaryFile.ts
+++ src/jestTransformerBinaryFile.ts
@@ -1,9 +1,10 @@
-// Excerpt from https://jestjs.io/docs/code-transformation#transforming-images-to-their-path:
-// > Importing [binary files like fonts or] images is a way to include them in your browser bundle, but they are
-// > not valid JavaScript. One way of handling it in Jest is to replace the imported value with its filename.
+import path from "node:path";
 
-import path from "path";
-
+/**
+ * Excerpt from https://jestjs.io/docs/code-transformation#transforming-images-to-their-path:
+ * > Importing [binary files like fonts or] images is a way to include them in your browser bundle, but they are
+ * > not valid JavaScript. One way of handling it in Jest is to replace the imported value with its filename.
+ */
 const jestTransformerBinaryFile = {
 	// Prefix the `sourceText` parameter with an underscore to tell TypeScript that the variable isn't being used.
 	process(_sourceText: string, sourcePath: string) {
