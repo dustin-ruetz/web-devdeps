26c1d8f5be1ce42d5f32ec75045165ab742cdec9
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-02-14 21:24:55 -0500

    refactor(vitest.config): switch to 'makeVitestConfig' function and mock out 'defineConfig' in test

--- src/__tests__/vitest.config.test.ts
+++ src/__tests__/vitest.config.test.ts
@@ -1,10 +1,20 @@
-import vitestConfig from "../vitest.config.js";
+import {makeVitestConfig} from "../vitest.config.js";
+
+jest.mock("vitest/config", () => ({
+	// Explicitly mock out `defineConfig` to prevent the following from being logged during the Jest test run:
+	// > warnCjsUsage - The CJS build of Vite's Node API is deprecated.
+	defineConfig: jest.fn(),
+}));
 
 test("it exports a configuration object", () => {
+	const vitestConfig = makeVitestConfig();
+
 	expect(typeof vitestConfig).toEqual("object");
 });
 
 test("the most important configuration options are correct", () => {
+	const vitestConfig = makeVitestConfig();
+
 	expect(vitestConfig.test?.coverage?.provider).toEqual("v8");
 	expect(vitestConfig.test?.globals).toBe(true);
 });
--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -8,34 +8,35 @@
 import {defineConfig, type UserConfig} from "vitest/config";
 
 /** https://vitest.dev/config/ */
-const vitestConfig: UserConfig = defineConfig({
-	test: {
-		exclude: ["**/lib/**", "**/node_modules/**"],
-		coverage: {
-			// Excerpt from https://vitest.dev/config/#coverage-exclude:
-			// > List of files excluded from coverage as glob patterns.
-			exclude: [
-				"lib/**",
-				// Vitest excludes certain configuration files by default; use a negated ignore pattern to include them in the coverage report.
-				"!src/*.config.*",
-			],
-			provider: "v8",
-			reporter: ["text", "text-summary"],
-			thresholds: {
-				"100": true,
+export const makeVitestConfig = (): UserConfig =>
+	({
+		test: {
+			exclude: ["**/lib/**", "**/node_modules/**"],
+			coverage: {
+				// Excerpt from https://vitest.dev/config/#coverage-exclude:
+				// > List of files excluded from coverage as glob patterns.
+				exclude: [
+					"lib/**",
+					// Vitest excludes certain configuration files by default; use a negated ignore pattern to include them in the coverage report.
+					"!src/*.config.*",
+				],
+				provider: "v8",
+				reporter: ["text", "text-summary"],
+				thresholds: {
+					"100": true,
+				},
 			},
+			// Excerpt from https://vitest.dev/config/#globals:
+			// > By default, `vitest` does not provide global APIs for explicitness. If you prefer to use the APIs globally
+			// > like Jest, you can pass the `--globals` option to CLI or add `globals: true` in the config.
+			//
+			// Excerpt from https://vitest.dev/guide/migration.html#globals-as-a-default:
+			// > Jest has their globals API enabled by default. Vitest does not. You can either enable globals via the
+			// > `globals` configuration setting or update your code to use imports from the `vitest` module instead.
+			// >
+			// > If you decide to keep globals disabled, be aware that common libraries like `testing-library` will not run auto DOM cleanup.
+			globals: true,
 		},
-		// Excerpt from https://vitest.dev/config/#globals:
-		// > By default, `vitest` does not provide global APIs for explicitness. If you prefer to use the APIs globally
-		// > like Jest, you can pass the `--globals` option to CLI or add `globals: true` in the config.
-		//
-		// Excerpt from https://vitest.dev/guide/migration.html#globals-as-a-default:
-		// > Jest has their globals API enabled by default. Vitest does not. You can either enable globals via the
-		// > `globals` configuration setting or update your code to use imports from the `vitest` module instead.
-		// >
-		// > If you decide to keep globals disabled, be aware that common libraries like `testing-library` will not run auto DOM cleanup.
-		globals: true,
-	},
-} as const);
+	}) as const;
 
-export default vitestConfig;
+export default defineConfig(makeVitestConfig());
