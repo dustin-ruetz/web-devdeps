d7536827333bcc45681c2bbfac5d9d599153fb16
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-03-03 22:48:46 -0500

    test(utils/ValidationError): refactor test to de-duplicate repetitive assertions

--- src/utils/ValidationError.test.ts
+++ src/utils/ValidationError.test.ts
@@ -1,8 +1,20 @@
 import {ValidationError} from "./ValidationError.js";
 
 describe("ValidationError is thrown correctly", () => {
+	const errorMessage = "error message";
+
+	test("with its cause object, code/message string, and name", () => {
+		const validationError = new ValidationError(errorMessage, {
+			cause: {code: "ERR_CODE"},
+		});
+
+		expect(validationError.cause).toStrictEqual({code: "ERR_CODE"});
+		expect(validationError.message).toEqual("ERR_CODE - error message");
+		expect(validationError.name).toEqual("ValidationError");
+	});
+
 	test("*with* a `values` object when it's provided", () => {
-		const validationError = new ValidationError("error message", {
+		const validationError = new ValidationError(errorMessage, {
 			cause: {code: "ERR_CODE_WITH_VALUES", values: {parameter: "argument"}},
 		});
 
@@ -10,23 +22,15 @@ describe("ValidationError is thrown correctly", () => {
 			code: "ERR_CODE_WITH_VALUES",
 			values: {parameter: "argument"},
 		});
-		expect(validationError.message).toEqual(
-			"ERR_CODE_WITH_VALUES - error message",
-		);
-		expect(validationError.name).toEqual("ValidationError");
 	});
 
 	test("*without* a `values` object when it's not provided", () => {
-		const validationError = new ValidationError("error message", {
+		const validationError = new ValidationError(errorMessage, {
 			cause: {code: "ERR_CODE_WITHOUT_VALUES"},
 		});
 
 		expect(validationError.cause).toStrictEqual({
 			code: "ERR_CODE_WITHOUT_VALUES",
 		});
-		expect(validationError.message).toEqual(
-			"ERR_CODE_WITHOUT_VALUES - error message",
-		);
-		expect(validationError.name).toEqual("ValidationError");
 	});
 });
