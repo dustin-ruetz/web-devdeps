9f6c088b1debf69e8c6bed8c9317ef98d1713e1d
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-03-03 20:56:50 -0500

    test(jest-transformers/svgFile): make 'xmlns' first attribute, add two more 'expect' assertions

--- src/jest-transformers/svgFile.test.ts
+++ src/jest-transformers/svgFile.test.ts
@@ -3,9 +3,9 @@ import jestTransformerSVGFile from "./svgFile.js";
 test('it transforms SVG files into `module.exports = "<svg>{...}</svg>"`', () => {
 	const svgSourceText = `
 <svg
+	xmlns="http://www.w3.org/2000/svg"
   aria-labelledby="svg-id"
   role="img"
-  xmlns="http://www.w3.org/2000/svg"
 >
   <title id="svg-id">SVG title.</title>
 </svg>
@@ -14,6 +14,8 @@ test('it transforms SVG files into `module.exports = "<svg>{...}</svg>"`', () =>
 	const svgTransformed = jestTransformerSVGFile.process(svgSourceText);
 
 	expect(svgTransformed.code).toContain("module.exports =");
+	expect(svgTransformed.code).toContain("<svg");
 	expect(svgTransformed.code).toContain("svg-id");
 	expect(svgTransformed.code).toContain("SVG title.");
+	expect(svgTransformed.code).toContain("</svg>");
 });
