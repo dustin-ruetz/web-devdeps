c97653ad6530e81a53888cf6d1023b4a8180912f
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-05-05 21:26:57 -0400

    fix(tsconfig.test): clean-up and simplify test descriptions

--- __tests__/tsconfig.test.ts
+++ __tests__/tsconfig.test.ts
@@ -1,26 +1,22 @@
 import tsConfig from "../tsconfig.json";
 
-test("all tsconfig files export a configuration object", () => {
+test("it exports a configuration object", () => {
 	expect(typeof tsConfig).toEqual("object");
 });
 
-test("the tsconfig file extends the @tsconfig/strictest configuration", () => {
+test("the most important configuration options are correct", () => {
 	expect(tsConfig.extends).toEqual("@tsconfig/strictest/tsconfig.json");
-});
+	expect(tsConfig.include).toStrictEqual(["./src/"]);
 
-describe("the most important compilerOptions are correct for:", () => {
-	test("tsconfig.json", () => {
-		const {compilerOptions} = tsConfig;
-
-		expect(compilerOptions.module).toEqual("NodeNext");
-		expect(compilerOptions.moduleResolution).toEqual("NodeNext");
-		expect(compilerOptions.outDir).toEqual("./lib/");
-		// Excerpt from https://www.typescriptlang.org/tsconfig#target on not using `"target": "esnext"`:
-		// > The special `ESNext` value refers to the highest version your version of TypeScript supports.
-		// > This setting should be used with caution, since it doesn't mean the same thing between
-		// > different TypeScript versions and can make upgrades less predictable.
-		expect(compilerOptions.target).not.toMatch(/esnext/i);
-		expect(compilerOptions.target).toEqual("ES2022");
-		expect(compilerOptions.verbatimModuleSyntax).toBe(true);
-	});
+	const {compilerOptions} = tsConfig;
+	expect(compilerOptions.module).toEqual("NodeNext");
+	expect(compilerOptions.moduleResolution).toEqual("NodeNext");
+	expect(compilerOptions.outDir).toEqual("./lib/");
+	// Excerpt from https://www.typescriptlang.org/tsconfig#target on not using `"target": "esnext"`:
+	// > The special `ESNext` value refers to the highest version your version of TypeScript supports.
+	// > This setting should be used with caution, since it doesn't mean the same thing between
+	// > different TypeScript versions and can make upgrades less predictable.
+	expect(compilerOptions.target).not.toMatch(/esnext/i);
+	expect(compilerOptions.target).toEqual("ES2022");
+	expect(compilerOptions.verbatimModuleSyntax).toBe(true);
 });
