c96a025df625b133c5bc15a7b06ea0a385d44ff0
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-05-05 21:39:36 -0400

    feat(tsconfig): move build-related configuration (from 'tsconfig.json' to 'tsconfig.build.json')

--- __tests__/tsconfig.build.test.ts
+++ __tests__/tsconfig.build.test.ts
@@ -7,4 +7,9 @@ test("it exports a configuration object", () => {
 test("the most important configuration options are correct", () => {
 	expect(tsConfigBuild.extends).toEqual("./tsconfig.json");
 	expect(tsConfigBuild.exclude).toStrictEqual(["**/*.mock.*", "**/*.test.*"]);
+
+	const {compilerOptions} = tsConfigBuild;
+	expect(compilerOptions.noEmitOnError).toBe(true);
+	expect(compilerOptions.outDir).toEqual("./lib/");
+	expect(compilerOptions.removeComments).toBe(true);
 });
--- __tests__/tsconfig.test.ts
+++ __tests__/tsconfig.test.ts
@@ -11,7 +11,6 @@ test("the most important configuration options are correct", () => {
 	const {compilerOptions} = tsConfig;
 	expect(compilerOptions.module).toEqual("NodeNext");
 	expect(compilerOptions.moduleResolution).toEqual("NodeNext");
-	expect(compilerOptions.outDir).toEqual("./lib/");
 	// Excerpt from https://www.typescriptlang.org/tsconfig#target on not using `"target": "esnext"`:
 	// > The special `ESNext` value refers to the highest version your version of TypeScript supports.
 	// > This setting should be used with caution, since it doesn't mean the same thing between
