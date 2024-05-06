8340c9c53c03f94a0558d91bd4eacc87da69daac
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-05-06 15:18:49 -0400

    docs(scripts/clean): clarify code comments and test description

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,8 +1,8 @@
 import {rm} from "node:fs/promises";
 
+/** Delete the lib/ directory where the compiled code is built to. */
 export const clean = async () => {
-	// Delete the lib/ directory.
-	// The following line simulates behavior of the Unix `rm -rf directoryName/` command,
+	// The following line simulates the behavior of the Unix `rm -rf directoryName/` command,
 	// but since this is a Node script this operation can be run in a cross-platform way.
 	await rm("lib/", {force: true, recursive: true});
 };
@@ -11,7 +11,7 @@ export const clean = async () => {
 // gets called in the test just from importing it. Since Jest automatically sets the `NODE_ENV=true` environment variable
 // when the test suite is run, use this as the conditional logic so that the `clean` function is only called via the
 // `npm run clean` script and not automatically when it's imported for the unit test.
-
+//
 // It's very difficult to test whether or not the `clean` function is called based on the value of `NODE_ENV`,
 // so use the special `v8` comment to exclude these lines from the test coverage report.
 // TODO: Post this issue as a question on Stack Overflow.
--- scripts/clean.test.js
+++ scripts/clean.test.js
@@ -3,7 +3,7 @@ import {clean} from "./clean.js";
 
 jest.mock("node:fs/promises");
 
-test("it removes the lib/ directory with the force and recursive options", async () => {
+test("it removes the lib/ directory with the 'force' and 'recursive' options", async () => {
 	await clean();
 
 	expect(rm).toHaveBeenCalledTimes(1);
