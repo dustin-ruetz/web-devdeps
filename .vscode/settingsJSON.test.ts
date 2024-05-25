7f079231f309b7a716d6212d14c638ca6100a297
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-05-24 22:50:49 -0400

    test(.vscode/settingsJSON): add missing assertion that VS Code setting JSON is an object

--- .vscode/settingsJSON.test.ts
+++ .vscode/settingsJSON.test.ts
@@ -1,6 +1,8 @@
 import vscodeSettingsJSON from "./settings.json";
 
 test("it is a configuration object and the most important config options are correct", () => {
+	expect(typeof vscodeSettingsJSON).toEqual("object");
+
 	// https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
 	expect(vscodeSettingsJSON["eslint.options"].ignorePath).toEqual(".gitignore");
 	expect(vscodeSettingsJSON["eslint.options"].overrideConfigFile).toEqual(
