5c8b7564b028334f2d94a7022ecdc9b961686a4e
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-05-27 21:48:42 -0400

    test(scripts/clean): use correct number of lines of code for 'v8 ignore next' directive

--- scripts/clean.js
+++ scripts/clean.js
@@ -24,7 +24,11 @@ const environments = {
 	 */
 	isTest: process.env.NODE_ENV === "test",
 };
-
+// Since it's difficult to test whether or not the `clean` function is called based on mocking environment variables,
+// use the special `v8 ignore next #` comment to exclude the following lines from the test coverage report.
+// TODO: Post this issue as a question on Stack Overflow.
+/* v8 ignore next 12 */
+//
 // Don't call the `clean` function in the CI/CD or test environments because:
 if (
 	// 1. The CI/CD environment will never have the lib/ directory because A) lib/ is in the .gitignore file,
@@ -33,10 +37,6 @@ if (
 	// 2. The unit test for this file imports the `clean` function to test it, but
 	//    the `clean` function itself should not be called during the test run.
 	!environments.isTest
-	// Since it's difficult to test whether or not the `clean` function is called based on the `NODE_ENV=test` env var,
-	// use the special `v8 ignore next #` comment to exclude the following lines from the test coverage report.
-	// TODO: Post this issue as a question on Stack Overflow.
-	/* v8 ignore next 3 */
 ) {
 	void clean();
 }
