6e277147cb7aacd4d6e8dc5067a3ca972762b992
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-06-01 21:40:15 -0400

    docs(semantic-release): clarify code comment

--- src/semantic-release.config.ts
+++ src/semantic-release.config.ts
@@ -29,7 +29,7 @@ const semanticReleaseConfig: Options = {
 					// > a _simplified_ format based on ISO 8601, which is always 24 characters long (`YYYY-MM-DDTHH:mm:ss.sssZ`). The timezone
 					// > is always UTC, as denoted by the suffix `Z`.
 					//
-					// Ignore the "unexpected template string expression" error since the variables in the message are replaced when committing.
+					// Ignore the "unexpected template string expression" lint error since the variables in the message are replaced when committing.
 					// eslint-disable-next-line no-template-curly-in-string
 					"chore(🤖 release): v${nextRelease.version} - <%= new Date().toISOString() %> [skip ci]\n\n${nextRelease.notes}",
 			},
