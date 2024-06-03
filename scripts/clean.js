57c700d4022b53e898bd4508582af0ea2b35fb97
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-06-02 22:03:22 -0400

    feat(scripts/clean): simplify 'clean' function and its associated unit test

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,6 +2,31 @@ import {rm} from "node:fs/promises";
 
 /** Delete the lib/ directory where the compiled code is built to. */
 export const clean = async () => {
+	const environments = {
+		/**
+		 * Excerpt from https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables:
+		 * > `CI` - Always set to `true`.
+		 */
+		isCI: process.env.CI === "true",
+		/**
+		 * Excerpt from https://jestjs.io/docs/environment-variables:
+		 * > `NODE_ENV` - Set to `"test"` if it's not already set to something else.
+		 */
+		isTest: process.env.NODE_ENV === "test",
+	};
+
+	// Return early to avoid `clean`ing in the CI/CD or test environments because:
+	if (
+		// 1. The CI/CD environment will never have the lib/ directory because A) lib/ is in the .gitignore file,
+		//    and B) GitHub Actions does a fresh Git checkout of the repo every time a workflow is run.
+		environments.isCI ||
+		// 2. The unit test for this file imports the `clean` function to test it, but without the following
+		//    conditional check the `clean` function itself gets called twice during the test run.
+		environments.isTest
+	) {
+		return;
+	}
+
 	const dirToDelete = "lib/";
 
 	// eslint-disable-next-line no-console
@@ -12,31 +37,4 @@ export const clean = async () => {
 	await rm(dirToDelete, {force: true, recursive: true});
 };
 
-const environments = {
-	/**
-	 * Excerpt from https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables:
-	 * > `CI` - Always set to `true`.
-	 */
-	isCI: process.env.CI === "true",
-	/**
-	 * Excerpt from https://jestjs.io/docs/environment-variables:
-	 * > `NODE_ENV` - Set to `"test"` if it's not already set to something else.
-	 */
-	isTest: process.env.NODE_ENV === "test",
-};
-// Since it's difficult to test whether or not the `clean` function is called based on mocking environment variables,
-// use the special `v8 ignore next #` comment to exclude the following lines from the test coverage report.
-// TODO: Post this issue as a question on Stack Overflow.
-/* v8 ignore next 12 */
-//
-// Don't call the `clean` function in the CI/CD or test environments because:
-if (
-	// 1. The CI/CD environment will never have the lib/ directory because A) lib/ is in the .gitignore file,
-	//    and B) GitHub Actions does a fresh Git checkout of the repo every time a workflow is run.
-	!environments.isCI &&
-	// 2. The unit test for this file imports the `clean` function to test it, but
-	//    the `clean` function itself should not be called during the test run.
-	!environments.isTest
-) {
-	void clean();
-}
+void clean();
--- scripts/clean.test.js
+++ scripts/clean.test.js
@@ -4,7 +4,31 @@ import {clean} from "./clean.js";
 jest.mock("node:fs/promises");
 const consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
 
-test("it removes the lib/ directory with the 'force' and 'recursive' options", async () => {
+describe("it *does not* remove the lib/ directory when in the", () => {
+	test("CI/CD environment", async () => {
+		process.env.CI = "true";
+
+		await clean();
+
+		expect(consoleLogSpy).not.toHaveBeenCalled();
+		expect(rm).not.toHaveBeenCalled();
+	});
+
+	test("test environment", async () => {
+		process.env.NODE_ENV = "test";
+
+		await clean();
+
+		expect(consoleLogSpy).not.toHaveBeenCalled();
+		expect(rm).not.toHaveBeenCalled();
+	});
+});
+
+test("it *does* remove the lib/ directory with the 'force' and 'recursive' options", async () => {
+	// Delete both of the relevant environment variables to make them `undefined`.
+	delete process.env.CI;
+	delete process.env.NODE_ENV;
+
 	await clean();
 
 	expect(consoleLogSpy).toHaveBeenCalledTimes(1);
