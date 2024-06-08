184ff933de17e4fc1d892e519e8e055f3d5d92f9
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-06-08 14:02:50 -0400

    feat(stylelint.config): modify config to accept conditional values for 'extends' and 'overrides'

--- src/stylelint.config.test.ts
+++ src/stylelint.config.test.ts
@@ -1,5 +1,54 @@
-import stylelintConfig from "./stylelint.config.js";
+import {dependsOnMock} from "./utils/dependsOn.mock.js";
+import {makeStylelintConfig} from "./stylelint.config.js";
 
-test("it exports a configuration object and the most important config options are correct", () => {
-	expect(typeof stylelintConfig).toEqual("object");
+describe("it exports a configuration object and the most important config options are correct", () => {
+	test("for the parts of the config that *are not* affected by conditional logic", async () => {
+		const stylelintConfig = await makeStylelintConfig();
+
+		expect(typeof stylelintConfig).toEqual("object");
+
+		expect(stylelintConfig.extends).toStrictEqual([
+			"stylelint-config-standard",
+		]);
+		expect(stylelintConfig.reportDescriptionlessDisables).toBe(true);
+		expect(stylelintConfig.reportInvalidScopeDisables).toBe(true);
+		expect(stylelintConfig.reportNeedlessDisables).toBe(true);
+		expect(stylelintConfig.rules?.["no-unknown-custom-properties"]).toBe(true);
+		expect(stylelintConfig.rules?.["declaration-no-important"]).toBe(true);
+		expect(stylelintConfig.rules?.["unit-disallowed-list"]).toStrictEqual([
+			"em",
+		]);
+		expect(stylelintConfig.rules?.["max-nesting-depth"]).toEqual(3);
+		expect(stylelintConfig.rules?.["font-weight-notation"]).toEqual("numeric");
+	});
+
+	test("when testing this web-dev-deps repo (which *does not* have frontend style-related dependencies)", async () => {
+		const hasFrontendStyleDependencies = false;
+		dependsOnMock.mockResolvedValue(hasFrontendStyleDependencies);
+
+		const stylelintConfig = await makeStylelintConfig();
+
+		expect(dependsOnMock).toHaveBeenCalledWith(["sass"]);
+		expect(dependsOnMock).toHaveBeenCalledWith(["styled-components"]);
+		expect(stylelintConfig.overrides).toStrictEqual([]);
+	});
+
+	test("when testing a repo that has installed the sass package", async () => {
+		const hasSassDependency = true;
+		dependsOnMock.mockResolvedValue(hasSassDependency);
+
+		const stylelintConfig = await makeStylelintConfig();
+
+		expect(stylelintConfig.extends).toEqual(
+			expect.arrayContaining(["stylelint-config-standard-scss"]),
+		);
+		expect(stylelintConfig.overrides).toEqual(
+			expect.arrayContaining([
+				{
+					customSyntax: "postcss-scss",
+					files: ["**/*.scss"],
+				},
+			]),
+		);
+	});
 });
--- src/stylelint.config.ts
+++ src/stylelint.config.ts
@@ -1,40 +1,63 @@
 import type {Config} from "stylelint";
+import {dependsOn} from "./utils/dependsOn.js";
 
 /** https://stylelint.io/user-guide/configure */
-const stylelintConfig: Config = {
-	extends: ["stylelint-config-standard", "stylelint-config-standard-scss"],
-	rules: {
-		// https://stylelint.io/user-guide/rules#avoid-errors
-		// > This rule considers the identifiers of `@keyframes` rules defined within the same source to be known.
-		"no-unknown-animations": true,
-		// > This rule considers custom media queries defined **within the same source** to be known.
-		"no-unknown-custom-media": true,
-		// > This rule considers custom properties defined within the same source to be known.
-		"no-unknown-custom-properties": true,
-		// https://stylelint.io/user-guide/rules#enforce-conventions
-		// > Disallow `!important` within declarations.
-		"declaration-no-important": true,
-		// > Limit the depth of nesting.
-		// > **Note**: root-level at-rules will **not be included** in the nesting depth calculation, because
-		// > most users would take for granted that root-level at-rules are "free" (since they're necessary).
-		"max-nesting-depth": 3,
-		// > Require numeric or named (where possible) `font-weight` values.
-		"font-weight-notation": "numeric",
-	},
+export const makeStylelintConfig = async (): Promise<Config> => {
+	const hasSassDependency = await dependsOn(["sass"]);
+	const hasStyledComponentsDependency = await dependsOn(["styled-components"]);
+
+	const overrides: Config["overrides"] = [];
 	// Use two plugins from https://stylelint.io/awesome-stylelint#custom-syntaxes
 	// to enable Stylelint to check Sass and styled-components code.
-	overrides: [
+	if (hasSassDependency) {
 		// https://marketplace.visualstudio.com/items?itemName=stylelint.vscode-stylelint#only-css-and-postcss-are-validated-by-default
-		{
+		overrides.push({
 			customSyntax: "postcss-scss",
 			files: ["**/*.scss"],
-		},
+		});
+	}
+	if (hasStyledComponentsDependency) {
 		// https://github.com/hudochenkov/postcss-styled-syntax#stylelint
-		{
+		overrides.push({
 			customSyntax: "postcss-styled-syntax",
 			files: ["**/*.{jsx,tsx}"],
-		},
-	],
-} as const;
+		});
+	}
 
-export default stylelintConfig;
+	return {
+		extends: [
+			"stylelint-config-standard",
+			// Conditionally extend the standard SCSS configuration if the consuming repository has a dependency on the `sass` package.
+			...(hasSassDependency ? ["stylelint-config-standard-scss"] : []),
+		],
+		// Excerpt from https://stylelint.io/user-guide/configure#report:
+		// > These `report*` properties provide extra validation for `stylelint-disable` comments.
+		// > This can help enforce useful and well-documented disables.
+		reportDescriptionlessDisables: true,
+		reportInvalidScopeDisables: true,
+		reportNeedlessDisables: true,
+		rules: {
+			// https://stylelint.io/user-guide/rules#avoid-errors
+			// > This rule considers the identifiers of `@keyframes` rules defined within the same source to be known.
+			"no-unknown-animations": true,
+			// > This rule considers custom media queries defined **within the same source** to be known.
+			"no-unknown-custom-media": true,
+			// > This rule considers custom properties defined within the same source to be known.
+			"no-unknown-custom-properties": true,
+			// https://stylelint.io/user-guide/rules#enforce-conventions
+			// > Disallow `!important` within declarations.
+			"declaration-no-important": true,
+			// > Specify a list of disallowed units.
+			"unit-disallowed-list": ["em"],
+			// > Limit the depth of nesting.
+			// > **Note**: root-level at-rules will **not be included** in the nesting depth calculation, because
+			// > most users would take for granted that root-level at-rules are "free" (since they're necessary).
+			"max-nesting-depth": 3,
+			// > Require numeric or named (where possible) `font-weight` values.
+			"font-weight-notation": "numeric",
+		},
+		overrides,
+	};
+};
+
+export default makeStylelintConfig();
