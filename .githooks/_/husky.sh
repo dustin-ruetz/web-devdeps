98279fe1434349c1b94df17da6fd61c2bc295552
Dustin Ruetz <26169612+dustin-ruetz@users.noreply.github.com>
2024-06-15 22:40:38 -0400

    feat: fix and improve husky script for Git hooks (#42)
    * fix(package.json): correct 'git config core.hooksPath' to './.githooks/_/'
    
    * docs(.githooks/_/husky): clarify code comment for incorporating Husky script changes
    
    * feat(.githooks/_/husky): augment Husky logs and error messages
    
    * docs(.githooks): move husky comments from shell script to dedicated README.md file

--- .githooks/_/husky.sh
+++ .githooks/_/husky.sh
@@ -1,49 +1,38 @@
 #!/usr/bin/env sh
 
-# Description   - husky.sh shell script adapted from the original husky template for Git hooks.
-# Documentation - https://typicode.github.io/husky/
-# Repository    - https://github.com/typicode/husky
-# Version       - 9.0.11
-#
-# To upgrade the husky version:
-#
-# 1. Run the following commands:
-#
-#    ```sh
-#    npm install --save-dev --save-exact husky@latest
-#    npx husky init
-#    npm run init
-#    ```
-#
-# 2. Compare the Git diffs between the following files and incorporate the changes from the former into the latter:
-#
-#    | Former                 | Latter              |
-#    | :--------------------- | :------------------ |
-#    | .githooks/_husky.sh    | .husky/_/h          |
-#    | .githooks/_/commit-msg | .husky/_/commit-msg |
-#    | .githooks/_/pre-commit | .husky/_/pre-commit |
-#    | .githooks/_/pre-push   | .husky/_/pre-push   |
-#
-# 3. Delete the .husky/ directory.
-#
-# 4. Commit the changes to the files in the .githooks/ directory.
+# Related file: .githooks/_/README.md
 
+# If the $HUSKY environment variable is set to 2 then make the shell
+# print the commands (and their arguments) as they're executed.
 [ "$HUSKY" = "2" ] && set -x
+# h = the name of the Git hook (ex: commit-msg)
 h="${0##*/}"
+# s = the path of the Git hook script (ex: .githooks/commit-msg)
 s="${0%/*/*}/$h"
 
-[ ! -f "$s" ] && exit 0
+# Only log the script if it doesn't contain the ".githooks/_/" substring.
+if [[ $s != *.githooks/_/* ]]; then
+  echo "🪝-ℹ️ Executing Git hook script: $s"
+fi
 
+# Check if the script exists; if it doesn't exist, log a warning and terminate with a zero status code.
+[ ! -f "$s" ] && echo "🪝-⚠️ Git hook script not found: $s" && exit 0
+
+# Check if the following files exist; if they do exist, source them.
 for f in "${XDG_CONFIG_HOME:-$HOME/.config}/husky/init.sh" "$HOME/.huskyrc"; do
 	# shellcheck disable=SC1090
 	[ -f "$f" ] && . "$f"
 done
 
+# If the $HUSKY environment variable is set to 0 then terminate with a zero status code.
 [ "${HUSKY-}" = "0" ] && exit 0
 
+# Execute the Git hook script file along with its passed arguments.
 sh -e "$s" "$@"
+# Store the exit status code.
 c=$?
 
-[ $c != 0 ] && echo "husky - $h script failed (code $c)"
-[ $c = 127 ] && echo "husky - command not found in PATH=$PATH"
+# If the Git hook script exits with a non-zero status code then log the error(s) and exit with the status code.
+[ $c != 0 ] && echo "🐶-❌ husky error - $h script failed (code $c)"
+[ $c = 127 ] && echo "🐶-❌ husky error - command not found in PATH=$PATH"
 exit $c
