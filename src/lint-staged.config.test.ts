23c657ea6fab22af5e0c4fec88e6b2b433ca43f4
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-06-18 22:09:00 -0400

    feat: change NPM script separator from '.' period to '/' forward slash

--- src/lint-staged.config.test.ts
+++ src/lint-staged.config.test.ts
@@ -17,7 +17,7 @@ describe("it runs the correct commands for", () => {
 
 	test("linting files with styles", () => {
 		expect(lintstagedConfig["*.{css,scss,jsx,tsx}" as keyof Config]).toEqual(
-			"npm run lint.styles -- --fix",
+			"npm run lint/styles -- --fix",
 		);
 	});
 
@@ -27,18 +27,18 @@ describe("it runs the correct commands for", () => {
 
 	test("linting JS and TS files", () => {
 		expect(lintJavaScriptTypeScriptCommand).toEqual(
-			`npm run lint.js-ts -- --fix ${relativePath}`,
+			`npm run lint/js-ts -- --fix ${relativePath}`,
 		);
 	});
 
 	test("typechecking", () => {
-		expect(typecheckCommand).toEqual("npm run check.types");
+		expect(typecheckCommand).toEqual("npm run check/types");
 	});
 
 	test("unit testing", () => {
 		// @ts-expect-error TODO: See above comment.
 		expect(lintstagedConfig["*.{js,jsx,json,ts,tsx}"](relativePaths)).toEqual(
-			`npm run test.unit.coverage -- --findRelatedTests --collectCoverageFrom= ${relativePath}`,
+			`npm run test/unit/coverage -- --findRelatedTests --collectCoverageFrom= ${relativePath}`,
 		);
 	});
 });
--- src/lint-staged.config.ts
+++ src/lint-staged.config.ts
@@ -3,28 +3,28 @@ import type {Config} from "lint-staged";
 /** https://github.com/lint-staged/lint-staged */
 const lintstagedConfig: Config = {
 	/**
-	 * Note: Run "format" and "lint.*" as their base scripts (i.e. _not_ "fix.format" and "fix.lint.*") because
+	 * Note: Run "format" and "lint/*" as their base scripts (i.e. _not_ "fix/format" and "fix/lint/*") because
 	 * lint-staged passes the staged file(s) by appending the path(s) as the last argument to the commands.
 	 */
 	// Fix code formatting for all file types that Prettier supports.
 	"*": "npm run format -- --ignore-unknown --write",
-	"*.{css,scss,jsx,tsx}": "npm run lint.styles -- --fix",
+	"*.{css,scss,jsx,tsx}": "npm run lint/styles -- --fix",
 	"*.{js,jsx,ts,tsx}": (relativePaths) => [
 		// Fix code issues for all file types that ESLint supports.
-		`npm run lint.js-ts -- --fix ${relativePaths.join(" ")}`,
-		// The "check.types" script runs the TypeScript compiler (tsc), but tsc ignores the tsconfig.json configuration file
+		`npm run lint/js-ts -- --fix ${relativePaths.join(" ")}`,
+		// The "check/types" script runs the TypeScript compiler (tsc), but tsc ignores the tsconfig.json configuration file
 		// due to the file paths that are appended to the command by lint-staged. Work around this limitation by
 		// typechecking the entire codebase, i.e. not just the staged files.
 		//
 		// Issues with more details:
 		// - https://github.com/lint-staged/lint-staged/issues/825
 		// - https://github.com/microsoft/TypeScript/issues/27379
-		"npm run check.types",
+		"npm run check/types",
 	],
 	// This repo has unit tests for JSON files, so include them in the glob pattern.
 	"*.{js,jsx,json,ts,tsx}": (relativePaths) => {
 		const jestCommand =
-			"npm run test.unit.coverage --" +
+			"npm run test/unit/coverage --" +
 			// Excerpt from https://jestjs.io/docs/cli#--findrelatedtests-spaceseparatedlistofsourcefiles:
 			// > Find and run the tests that cover a space-separated list of source files that were passed in as arguments.
 			// > Useful for pre-commit hook integration to run the minimal amount of tests necessary.
