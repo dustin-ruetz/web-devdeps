292726a2089c002cc4af8db68c93097dc5c7aaf1
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-08-05 21:38:01 -0400

    test(.node-version): simplify test description

--- __tests__/.node-version.test.ts
+++ __tests__/.node-version.test.ts
@@ -1,6 +1,6 @@
 import {readFile} from "fs/promises";
 
-test("that the correct version of Node.js is specified", async () => {
+test("the correct version of Node.js is specified", async () => {
 	const versionNumber = (
 		await readFile(".node-version", {encoding: "utf-8"})
 	).trim();
