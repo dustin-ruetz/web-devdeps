7188eb416690712b3ef99aa2dfc3dc504e2c0462
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-08-11 20:56:36 -0400

    feat(scripts/helpers/initRepoHelpers): initial 'initRepoHelpers' functions and unit tests

---
+++ src/scripts/helpers/initRepoHelpers.test.ts
@@ -0,0 +1,240 @@
+import {mkdir, readdir, readFile, writeFile} from "node:fs/promises";
+import {
+	writeGitAttributes,
+	writeGitHooks,
+	writeGitIgnore,
+	writeLicense,
+	writeNodeVersion,
+	writePackageJson,
+	writeReadme,
+	writeTsConfig,
+	writeTsConfigBuild,
+	writeVsCodeSettings,
+} from "./initRepoHelpers.js";
+
+jest.mock("node:fs/promises", () => ({
+	mkdir: jest.fn(),
+	readdir: jest.fn(),
+	readFile: jest.fn(),
+	writeFile: jest.fn(),
+}));
+// Paraphrased excerpt from https://www.mikeborozdin.com/post/changing-jest-mocks-between-tests:
+// > Typecast the imported mocked module into a mocked function with writeable properties.
+const mockReaddir = readdir as jest.MockedFunction<typeof readdir>;
+const mockReadFile = readFile as jest.MockedFunction<typeof readFile>;
+
+beforeAll(() => {
+	jest.spyOn(console, "log").mockImplementation();
+});
+
+afterEach(() => {
+	jest.clearAllMocks();
+});
+
+test("writeGitAttributes", async () => {
+	await writeGitAttributes();
+
+	expect(writeFile).toHaveBeenCalledTimes(1);
+	expect(writeFile).toHaveBeenCalledWith(
+		expect.stringContaining(".gitattributes"),
+		expect.stringContaining("* text=auto"),
+	);
+});
+
+test("writeGitHooks", async () => {
+	const githooksDirectoryContents = [
+		"_",
+		"commit-msg",
+		"pre-commit",
+		"pre-push",
+	] as const;
+	// @ts-expect-error TODO: Figure out how to correctly type the promise's resolved value here.
+	mockReaddir.mockResolvedValue(githooksDirectoryContents);
+	// Filter out the "_" underscore directory since it contains the husky-specific script files.
+	const githooksFiles = githooksDirectoryContents.filter(
+		(item) => item !== "_",
+	);
+
+	await writeGitHooks();
+
+	githooksFiles.forEach((githook, index) => {
+		/** The path of the filename to write. */
+		const file = `.githooks/${githook}`;
+		/** The contents to write to the file. */
+		const contents = `./node_modules/@dustin-ruetz/devdeps/.githooks/_/${githook}`;
+
+		expect(mkdir).toHaveBeenCalledTimes(1);
+		expect(mkdir).toHaveBeenCalledWith(expect.stringContaining(".githooks/"));
+		expect(writeFile).toHaveBeenNthCalledWith(
+			index + 1,
+			expect.stringContaining(file),
+			expect.stringContaining(contents),
+		);
+	});
+});
+
+test("writeGitIgnore", async () => {
+	await writeGitIgnore();
+
+	expect(writeFile).toHaveBeenCalledTimes(1);
+	expect(writeFile).toHaveBeenCalledWith(
+		expect.stringContaining(".gitignore"),
+		expect.stringContaining("node_modules/"),
+	);
+});
+
+test("writeLicense", async () => {
+	mockReadFile.mockResolvedValue("Copyright (c) 1999 Dustin Ruetz");
+	const currentYear = new Date().getFullYear();
+
+	await writeLicense();
+
+	expect(readFile).toHaveBeenCalledTimes(1);
+	expect(readFile).toHaveBeenCalledWith(expect.stringContaining("LICENSE"), {
+		encoding: "utf-8",
+	});
+	expect(writeFile).toHaveBeenCalledTimes(1);
+	expect(writeFile).toHaveBeenCalledWith(
+		expect.stringContaining("LICENSE"),
+		`Copyright (c) ${currentYear} Dustin Ruetz`,
+	);
+});
+
+test("writeNodeVersion", async () => {
+	await writeNodeVersion("20");
+
+	expect(writeFile).toHaveBeenCalledTimes(1);
+	expect(writeFile).toHaveBeenCalledWith(
+		expect.stringContaining(".node-version"),
+		"20",
+	);
+});
+
+describe("writePackageJson", () => {
+	/** Simplified mock of the file contents from a `package.json` file. */
+	const mockPackageJsonContents = `
+{
+		"name": "",
+		"version": "",
+		"description": "",
+		"scripts": {
+			"script": "package --config ./lib/package.config.js"
+		},
+		"files": [],
+		"repository": {
+			"url": ""
+		},
+		"publishConfig": {},
+		"dependencies": {},
+		"devDependencies": {}
+}
+`;
+	/** Generic script representing a library named `package` that is called with its associated `package.config.js` configuration file. */
+	const genericScript =
+		"package --config ./node_modules/@dustin-ruetz/devdeps/lib/package.config.js";
+	/** Script representing the Stylelint library getting called with its associated configuration file. */
+	const lintStylesScript =
+		"stylelint --config ./node_modules/@dustin-ruetz/devdeps/lib/stylelint.config.js";
+
+	test("it *does not* configure Stylelint", async () => {
+		mockReadFile.mockResolvedValue(mockPackageJsonContents);
+
+		await writePackageJson("repo-name", false);
+
+		expect(readFile).toHaveBeenCalledTimes(1);
+		expect(readFile).toHaveBeenCalledWith(
+			expect.stringContaining("package.json"),
+			{encoding: "utf-8"},
+		);
+
+		expect(writeFile).toHaveBeenCalledTimes(1);
+		expect(writeFile).toHaveBeenCalledWith(
+			expect.stringContaining("package.json"),
+			expect.stringMatching(new RegExp(genericScript)),
+		);
+		expect(writeFile).toHaveBeenCalledWith(
+			expect.stringContaining("package.json"),
+			expect.not.stringMatching(new RegExp(lintStylesScript)),
+		);
+	});
+
+	test("it *does* configure Stylelint", async () => {
+		mockReadFile.mockResolvedValue(mockPackageJsonContents);
+
+		await writePackageJson("repo-name", true);
+
+		expect(readFile).toHaveBeenCalledTimes(1);
+		expect(readFile).toHaveBeenCalledWith(
+			expect.stringContaining("package.json"),
+			{encoding: "utf-8"},
+		);
+
+		expect(writeFile).toHaveBeenCalledTimes(1);
+		expect(writeFile).toHaveBeenCalledWith(
+			expect.stringContaining("package.json"),
+			expect.stringMatching(
+				// Regular expression adapted from Michael Socha's 2014-Nov-18 answer to the following question posted on Stack Overflow:
+				// https://stackoverflow.com/questions/2219830/regular-expression-to-find-two-strings-anywhere-in-input/27005678#27005678
+				new RegExp(`(${genericScript}(.|\n)*${lintStylesScript})`),
+			),
+		);
+	});
+});
+
+test("writeReadme", async () => {
+	await writeReadme("repo-name");
+
+	expect(writeFile).toHaveBeenCalledTimes(1);
+	expect(writeFile).toHaveBeenCalledWith(
+		expect.stringContaining("README.md"),
+		"# repo-name",
+	);
+});
+
+test("writeTsConfigBuild", async () => {
+	await writeTsConfigBuild();
+
+	expect(writeFile).toHaveBeenCalledTimes(1);
+	expect(writeFile).toHaveBeenCalledWith(
+		expect.stringContaining("tsconfig.build.json"),
+		expect.stringContaining(
+			"./node_modules/@dustin-ruetz/devdeps/tsconfig.build.json",
+		),
+	);
+});
+
+test("writeTsConfig", async () => {
+	await writeTsConfig();
+
+	expect(writeFile).toHaveBeenCalledTimes(1);
+	expect(writeFile).toHaveBeenCalledWith(
+		expect.stringContaining("tsconfig.json"),
+		expect.stringContaining(
+			"./node_modules/@dustin-ruetz/devdeps/tsconfig.json",
+		),
+	);
+});
+
+describe("writeVsCodeSettings", () => {
+	test("it *does not* configure Stylelint", async () => {
+		await writeVsCodeSettings(false);
+
+		expect(writeFile).toHaveBeenCalledTimes(1);
+		expect(writeFile).toHaveBeenCalledWith(
+			expect.stringContaining(".vscode/settings.json"),
+			expect.not.stringContaining("stylelint.configFile"),
+		);
+	});
+
+	test("it *does* configure Stylelint", async () => {
+		await writeVsCodeSettings(true);
+
+		expect(mkdir).toHaveBeenCalledTimes(1);
+		expect(mkdir).toHaveBeenCalledWith(expect.stringContaining(".vscode/"));
+		expect(writeFile).toHaveBeenCalledTimes(1);
+		expect(writeFile).toHaveBeenCalledWith(
+			expect.stringContaining(".vscode/settings.json"),
+			expect.stringContaining("stylelint.configFile"),
+		);
+	});
+});
---
+++ src/scripts/helpers/initRepoHelpers.ts
@@ -0,0 +1,213 @@
+import {mkdir, readdir, readFile, writeFile} from "node:fs/promises";
+import {getRepoMetadata} from "../../utils/getRepoMetadata.js";
+import {getRootPaths} from "./getRootPaths.js";
+
+const {dependencyPartialPath} = getRepoMetadata();
+
+const rootPathToReadFrom = getRootPaths().readFrom;
+const rootPathToWriteTo = getRootPaths().writeTo;
+
+/** Encoding to use for Node.js file system operations. */
+const encoding = {encoding: "utf-8"} as const;
+
+/**
+ * Write a new `.gitattributes` file to disk and pre-populate it with a list
+ * of file extensions common to web-/Node.js-based projects.
+ */
+export const writeGitAttributes = async () => {
+	await writeFile(
+		`${rootPathToWriteTo}/.gitattributes`,
+		`
+* text=auto
+
+*.css  text
+*.js   text
+*.jsx  text
+*.json text
+*.md   text
+*.scss text
+*.svg  text
+*.ts   text
+*.tsx  text
+*.yaml text
+
+*.jpg binary
+*.png binary
+`.trimStart(),
+	);
+};
+
+/** Write a new `.githooks/` folder to disk, then write the new Git hook files to it. */
+export const writeGitHooks = async () => {
+	const githooksDirectoryContents = await readdir(
+		`${rootPathToReadFrom}/.githooks/`,
+		encoding,
+	);
+
+	// Filter out the "_" underscore directory since it contains the husky-specific script files.
+	const githooksFiles = githooksDirectoryContents.filter(
+		(item) => item !== "_",
+	);
+
+	await mkdir(`${rootPathToWriteTo}/.githooks/`);
+	await Promise.all(
+		githooksFiles.map((githook) =>
+			writeFile(
+				`${rootPathToWriteTo}/.githooks/${githook}`,
+				`
+#!/usr/bin/env sh
+./${dependencyPartialPath}/.githooks/_/${githook}
+`.trimStart(),
+			),
+		),
+	);
+};
+
+/** Write a new `.gitignore` file to disk and pre-populate it with the list of common ignore patterns. */
+export const writeGitIgnore = async () => {
+	await writeFile(
+		`${rootPathToWriteTo}/.gitignore`,
+		`
+.caches/
+node_modules/
+`.trimStart(),
+	);
+};
+
+/** Read the `LICENSE` file, update the copyright year, then write the file to disk. */
+export const writeLicense = async () => {
+	const fileContents = await readFile(
+		`${rootPathToReadFrom}/LICENSE`,
+		encoding,
+	);
+	// Replace the four-digit year in the "Copyright (c) yyyy" substring with the current year.
+	const fileContentsWithCurrentYear = fileContents.replace(
+		/\d{4}/,
+		new Date().getFullYear().toString(),
+	);
+	await writeFile(`${rootPathToWriteTo}/LICENSE`, fileContentsWithCurrentYear);
+};
+
+/** Write a new `.node-version` file to disk. */
+export const writeNodeVersion = async (nodeVersion: string) => {
+	await writeFile(`${rootPathToWriteTo}/.node-version`, nodeVersion);
+};
+
+/** Read the `package.json` file, update its contents, then write the file to disk. */
+export const writePackageJson = async (
+	repoName: string,
+	configureStylelint: boolean,
+) => {
+	const packageJsonContents = await readFile(
+		`${rootPathToReadFrom}/package.json`,
+		encoding,
+	);
+	const packageJson = JSON.parse(packageJsonContents);
+
+	const devdepsVersion = packageJson.version;
+
+	packageJson.name = repoName;
+	packageJson.version = "0.0.0";
+	packageJson.description = "";
+
+	packageJson.scripts = Object.entries(packageJson.scripts).reduce(
+		(
+			scriptsObject: {[scriptKey: string]: string},
+			[scriptKey, scriptValue],
+		) => {
+			/**
+			 * The following `String` conversion prevents TypeScript from reporting the following error:
+			 * > `"scriptValue" is of type "unknown". ts(18046)`
+			 *
+			 * This is a safe conversion because `packageJson.scripts` will _always_ be an object of scripts
+			 * that are in the format of key/value strings, so convert it here to keep TypeScript happy.
+			 */
+			const scriptValueString = String(scriptValue);
+
+			let updatedScriptValue = scriptValueString;
+			if (scriptValueString.includes("--config ./lib/")) {
+				updatedScriptValue = scriptValueString.replace(
+					"--config ./",
+					`--config ./${dependencyPartialPath}/`,
+				);
+			}
+			scriptsObject[scriptKey] = updatedScriptValue;
+			return scriptsObject;
+		},
+		{},
+	);
+	if (configureStylelint) {
+		packageJson.scripts["lint/styles"] =
+			`stylelint --config ./${dependencyPartialPath}/lib/stylelint.config.js --cache --cache-location ./.caches/.stylelintcache --ignore-path ./.gitignore`;
+		packageJson.scripts["check/lint/styles"] =
+			"npm run lint/styles -- '**/*.{css,scss,jsx,tsx}'";
+		packageJson.scripts["fix/lint/styles"] =
+			"npm run lint/styles -- --fix '**/*.{css,scss,jsx,tsx}'";
+	}
+
+	packageJson.files = [];
+	packageJson.repository.url = packageJson.repository.url.replace(
+		"devdeps",
+		repoName,
+	);
+	delete packageJson.publishConfig;
+	packageJson.dependencies = {};
+	packageJson.devDependencies = {
+		"@dustin-ruetz/devdeps": devdepsVersion,
+	};
+
+	await writeFile(
+		`${rootPathToWriteTo}/package.json`,
+		JSON.stringify(packageJson),
+	);
+};
+
+/** Write a new `README.md` file to disk and set the header to the name of the repository. */
+export const writeReadme = async (repoName: string) => {
+	await writeFile(`${rootPathToWriteTo}/README.md`, `# ${repoName}`);
+};
+
+/** Write a new `tsconfig.build.json` file to disk and make it extend this repo's configuration. */
+export const writeTsConfigBuild = async () => {
+	await writeFile(
+		`${rootPathToWriteTo}/tsconfig.build.json`,
+		`{"extends": "./${dependencyPartialPath}/tsconfig.build.json"}`,
+	);
+};
+
+/** Write a new `tsconfig.json` file to disk and make it extend this repo's configuration. */
+export const writeTsConfig = async () => {
+	await writeFile(
+		`${rootPathToWriteTo}/tsconfig.json`,
+		`{"extends": "./${dependencyPartialPath}/tsconfig.json"}`,
+	);
+};
+
+/** Write a new `.vscode/` folder to disk, then write a new `settings.json` file to it. */
+export const writeVsCodeSettings = async (configureStylelint: boolean) => {
+	const vsCodeSettingsJson: {[key: string]: unknown} = {
+		"eslint.options": {
+			ignorePath: ".gitignore",
+			overrideConfigFile: `${dependencyPartialPath}/lib/eslint.config.cjs`,
+		},
+		"prettier.configPath": `${dependencyPartialPath}/lib/prettier.config.js`,
+		"prettier.ignorePath": ".gitignore",
+	};
+
+	if (configureStylelint) {
+		vsCodeSettingsJson["stylelint.configFile"] =
+			`${dependencyPartialPath}/lib/stylelint.config.js`;
+		vsCodeSettingsJson["stylelint.validate"] = [
+			"css",
+			"javascriptreact",
+			"scss",
+			"typescriptreact",
+		];
+	}
+
+	await mkdir(`${rootPathToWriteTo}/.vscode/`);
+	await writeFile(
+		`${rootPathToWriteTo}/.vscode/settings.json`,
+		JSON.stringify(vsCodeSettingsJson),
+	);
+};
