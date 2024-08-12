f5bfcec796527f40308ace5541df35ffc3a222ef
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-08-11 22:18:25 -0400

    feat(src/index): initial 'runScript' index and unit tests

---
+++ src/index.test.ts
@@ -0,0 +1,109 @@
+import {initRepo, logInitRepoHelpText} from "./scripts/initRepo.js";
+import {runScript} from "./index.js";
+
+jest.mock("./scripts/initRepo.js");
+
+afterEach(() => {
+	jest.clearAllMocks();
+});
+
+const initialProcessArgv = process.argv;
+afterAll(() => {
+	process.argv = initialProcessArgv;
+});
+
+describe("`initRepo` script", () => {
+	const processArgv = ["_execPath", "_filePath", "init-repo"] as const;
+
+	beforeEach(() => {
+		process.argv = [...processArgv];
+	});
+
+	describe("logs the help text", () => {
+		test("when no arguments are passed", async () => {
+			await runScript();
+
+			expect(logInitRepoHelpText).toHaveBeenCalledTimes(1);
+			expect(initRepo).not.toHaveBeenCalled();
+		});
+
+		test("when the `--help` argument is passed", async () => {
+			process.argv.push("--help");
+
+			await runScript();
+
+			expect(logInitRepoHelpText).toHaveBeenCalledTimes(1);
+			expect(initRepo).not.toHaveBeenCalled();
+		});
+
+		test("when the `-h` argument is passed", async () => {
+			process.argv.push("-h");
+
+			await runScript();
+
+			expect(logInitRepoHelpText).toHaveBeenCalledTimes(1);
+			expect(initRepo).not.toHaveBeenCalled();
+		});
+	});
+
+	describe("is called with the correct arguments", () => {
+		test("when the required `repoName` argument is passed as an empty string", async () => {
+			process.argv.push("");
+
+			await runScript();
+
+			expect(logInitRepoHelpText).not.toHaveBeenCalled();
+			expect(initRepo).toHaveBeenCalledTimes(1);
+			expect(initRepo).toHaveBeenCalledWith("", []);
+		});
+
+		test("when the required `repoName` argument is passed *and* the optional flags are not passed", async () => {
+			process.argv.push("repo-name");
+
+			await runScript();
+
+			expect(logInitRepoHelpText).not.toHaveBeenCalled();
+			expect(initRepo).toHaveBeenCalledTimes(1);
+			expect(initRepo).toHaveBeenCalledWith("repo-name", []);
+		});
+
+		test("when the required `repoName` argument is passed *and* the optional flags are passed", async () => {
+			process.argv.push(
+				"repo-name",
+				"--configure-stylelint",
+				"--node-version=20",
+			);
+
+			await runScript();
+
+			expect(logInitRepoHelpText).not.toHaveBeenCalled();
+			expect(initRepo).toHaveBeenCalledTimes(1);
+			expect(initRepo).toHaveBeenCalledWith("repo-name", [
+				"--configure-stylelint",
+				"--node-version=20",
+			]);
+		});
+	});
+});
+
+describe("throws an error", () => {
+	const processArgv = ["_execPath", "_filePath"] as const;
+
+	beforeEach(() => {
+		process.argv = [...processArgv];
+	});
+
+	test("when no script name is passed", () => {
+		expect(async () => {
+			await runScript();
+		}).rejects.toThrow(/ERR_UNKNOWN_SCRIPT/);
+	});
+
+	test("when an unknown script is passed", () => {
+		process.argv.push("unknown-script");
+
+		expect(async () => {
+			await runScript();
+		}).rejects.toThrow(/ERR_UNKNOWN_SCRIPT/);
+	});
+});
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+import {initRepo, logInitRepoHelpText} from "./scripts/initRepo.js";
+import {ValidationError} from "./utils/ValidationError.js";
+
+/** Function that serves as the entrypoint file for runnable Node.js CLI scripts from this `@dustin-ruetz/devdeps` package. */
+export const runScript = async () => {
+	const [
+		_execPath, // executor - the Node.js executable
+		_filePath, // ignoredBin - ./lib/ (i.e. the path to the JavaScript file being executed)
+		script, // the name of the script to run
+		...args // all arguments that follow the script name
+	] = process.argv;
+
+	/** List of known scripts. */
+	const scripts = {
+		initRepo: "init-repo",
+	} as const;
+
+	switch (script) {
+		case scripts.initRepo: {
+			const hasHelpFlag = Boolean(
+				args.find((arg) => arg === "--help" || arg === "-h"),
+			);
+
+			// If either 1) no arguments are passed, or 2) one of the `help` flags are passed, then:
+			if (args.length === 0 || hasHelpFlag) {
+				// 1. Log the help text for the command.
+				logInitRepoHelpText();
+				// 2. Terminate the script.
+				break;
+			}
+
+			// The repository name is the only required argument and it has to be the first one, so:
+			// 1. `shift` it out of the `args` array if it's defined; or
+			// 2. Set it to an empty string if it's `undefined` and let the `initRepo` function throw an error.
+			const repoName = args.shift() || "";
+			await initRepo(repoName, args);
+			break;
+		}
+		default: {
+			throw new ValidationError(
+				`Unknown script: ${script}. Known scripts are: [${Object.values(scripts).join(", ")}]`,
+				{
+					cause: {
+						code: "ERR_UNKNOWN_SCRIPT",
+						values: {script},
+					},
+				},
+			);
+		}
+	}
+};
+
+// The unit test for this file imports the `runScript` function to test it, but the problem with this
+// is that `runScript` gets called in the test just from importing it. Since Jest automatically sets
+// the `NODE_ENV=true` environment variable when the test suite is run, use this as the conditional
+// logic to prevent `runScript` from being automatically called when it's imported for the unit test.
+//
+// It's very difficult to test whether or not the `runScript` function is called based on the value of `NODE_ENV`,
+// so use the special `v8 ignore next #` comment to exclude these lines from the test coverage report.
+//
+/* v8 ignore next 3 */
+if (process.env["NODE_ENV"] !== "test") {
+	runScript();
+}
