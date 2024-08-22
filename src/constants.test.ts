aa391d8f343c2f0eb69697a8bf297aac2e67799d
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-08-21 20:04:24 -0400

    feat(constants): initial constants file and unit test

---
+++ src/constants.test.ts
@@ -0,0 +1,11 @@
+import {
+	packageName,
+	nodeModulesPackagePath,
+	packageScope,
+} from "./constants.js";
+
+test("the constants are set to the correct values", () => {
+	expect(packageScope).toEqual("@dustin-ruetz");
+	expect(packageName).toEqual("devdeps");
+	expect(nodeModulesPackagePath).toEqual("node_modules/@dustin-ruetz/devdeps");
+});
---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+/** NPM User-scoped namespace. */
+export const packageScope = "@dustin-ruetz";
+
+/** NPM package name. */
+export const packageName = "devdeps";
+
+/** Relative path to package when installed as a dependency in a consuming project's `node_modules/` directory. */
+export const nodeModulesPackagePath = `node_modules/${packageScope}/${packageName}`;
