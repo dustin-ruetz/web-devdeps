83633c677fc046e7b33c9c733c0d162c685f03b4
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-08-21 21:49:59 -0400

    fix: add 'v8 ignore next #' comments to work around erroneous 'Uncovered Line #s' errors

--- src/utils/getIsDevDepsRepo.ts
+++ src/utils/getIsDevDepsRepo.ts
@@ -2,4 +2,9 @@ import {packageName} from "../constants.js";
 
 /** Given a path, determine whether or not it's this `devdeps` repository. */
 export const getIsDevDepsRepo = (path: string) =>
+	/**
+	 * @todo Figure out why the following conditional logic (which _is_ in fact covered in the unit test)
+	 *       erroneously and flakily reports "Uncovered Line #s" errors in the test coverage report.
+	 */
+	/* v8 ignore next 1 */
 	path.endsWith(packageName) || path.endsWith(`${packageName}/`);
