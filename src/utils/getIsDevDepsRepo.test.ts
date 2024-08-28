468d3e51bd2b254cceac26e6833249e68167900f
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-08-28 17:45:57 -0400

    test(utils/getIsDevDepsRepo): add assertion for empty string

--- src/utils/getIsDevDepsRepo.test.ts
+++ src/utils/getIsDevDepsRepo.test.ts
@@ -3,6 +3,7 @@ import {getIsDevDepsRepo} from "./getIsDevDepsRepo.js";
 describe("it correctly identifies whether or not the path indicates ", () => {
 	test.each`
 		path                                       | isDevDepsRepo
+		${""}                                      | ${false}
 		${"/Users/username/repos/devdeps/"}        | ${true}
 		${"/Users/username/repos/devdeps"}         | ${true}
 		${"/Users/username/repos/consuming-repo/"} | ${false}
