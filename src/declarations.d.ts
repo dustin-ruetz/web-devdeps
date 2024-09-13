4932808c5bf2fa909fca4a17f00edfd737b668ad
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-09-12 20:39:46 -0400

    fix(declarations.d.ts): improve '@eslint/js' type declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,9 +3,20 @@
  * the `@types/eslint__js` package by declaring the shape of the `@eslint/js` default module export here.
  */
 declare module "@eslint/js" {
-	export default {
+	/**
+	 * @todo Figure out how to use ESLint's `Linter.RulesRecord` type for the `rules` configuration objects.
+	 * @link https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/eslint__js/index.d.ts
+	 */
+	const js = {
 		configs: {
-			recommended: {},
+			all: {
+				rules: {},
+			},
+			recommended: {
+				rules: {},
+			},
 		},
-	};
+	} as const;
+
+	export default js;
 }
