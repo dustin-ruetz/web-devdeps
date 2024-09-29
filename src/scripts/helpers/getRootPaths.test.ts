138b0cdf9144adfdb2714e7d7333a1b0631c5b9a
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-09-29 16:30:01 -0400

    test(scripts/helpers/getRootPaths): rectify 'typescript-eslint/unbound-method' error

--- src/scripts/helpers/getRootPaths.test.ts
+++ src/scripts/helpers/getRootPaths.test.ts
@@ -5,31 +5,26 @@ afterEach(() => {
 	jest.clearAllMocks();
 });
 
-/**
- * @see {@link https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md}
- * @todo Ideally this `unbound-method` lint error should be handled by the `eslint-plugin-jest` package.
- */
-// eslint-disable-next-line @typescript-eslint/unbound-method
-const initialProcessCwd = process.cwd;
+const initialProcessCwd = process.cwd();
 afterAll(() => {
-	process.cwd = initialProcessCwd;
+	process.cwd = () => initialProcessCwd;
 });
 
 describe("the returned root paths (1. to read from, and 2. to write to) are correct when called from", () => {
-	test("this devdeps repo", () => {
+	it("this devdeps repo", () => {
 		getAbsoluteRepoRootPathMock.mockReturnValue(
 			"/Users/username/repos/devdeps",
 		);
 
 		const rootPaths = getRootPaths();
 
-		expect(rootPaths.readFrom).toEqual("/Users/username/repos/devdeps");
-		expect(rootPaths.writeTo).toEqual(
+		expect(rootPaths.readFrom).toBe("/Users/username/repos/devdeps");
+		expect(rootPaths.writeTo).toBe(
 			"/Users/username/repos/devdeps/.init-repo-script-test-output",
 		);
 	});
 
-	test("a consuming repo that has installed the devdeps package", () => {
+	it("a consuming repo that has installed the devdeps package", () => {
 		const consumingRepoAbsoluteRootDir = "/Users/username/repos/consuming-repo";
 		getAbsoluteRepoRootPathMock.mockReturnValue(consumingRepoAbsoluteRootDir);
 
@@ -37,9 +32,9 @@ describe("the returned root paths (1. to read from, and 2. to write to) are corr
 
 		const rootPaths = getRootPaths();
 
-		expect(rootPaths.readFrom).toEqual(
+		expect(rootPaths.readFrom).toBe(
 			"/Users/username/repos/consuming-repo/node_modules/@dustin-ruetz/devdeps",
 		);
-		expect(rootPaths.writeTo).toEqual("/Users/username/repos/consuming-repo");
+		expect(rootPaths.writeTo).toBe("/Users/username/repos/consuming-repo");
 	});
 });
