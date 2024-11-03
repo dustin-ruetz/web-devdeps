5663c3e67917da226293fb334892fd87157a4054
Dustin Ruetz <dtj.ruetz@gmail.com>
2024-11-03 08:54:35 -0500

    feat(modules.d.ts): remove manual declaration of '@eslint/js' since it now provides its own types

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,23 +1,6 @@
 /**
- * @description Fix `typescript-eslint` errors on the `eslintjs.configs.recommended` config object and avoid the need to install
- *              the `@types/eslint__js` package by declaring the shape of the `@eslint/js` default module export here.
- * @see {@link https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/eslint__js/index.d.ts}
  * @todo Figure out how to use ESLint's `Linter.RulesRecord` type for the `rules` configuration objects.
  */
-declare module "@eslint/js" {
-	const js = {
-		configs: {
-			all: {
-				rules: {},
-			},
-			recommended: {
-				rules: {},
-			},
-		},
-	} as const;
-
-	export default js;
-}
 
 /** @description The `eslint-plugin-jest` package doesn't have a DefinitelyTyped entry, so declare its shape here. */
 declare module "eslint-plugin-jest" {
