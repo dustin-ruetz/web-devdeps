e106933fb04dab652ab25c53053fac3f9aa41042
Dustin Ruetz <dtj.ruetz@gmail.com>
2025-05-01 19:36:30 -0400

    docs(constants): update JSDoc code comment for 'nodeModulesPackagePath' variable

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 export const packageName = "web-devdeps";
 
 /**
- * Relative flat/hoisted path to package when it's installed as a
+ * Relative flattened/hoisted path to package when it's installed as a
  * dependency in a consuming project's `node_modules/` directory.
  */
 export const nodeModulesPackagePath = `node_modules/${packageName}`;
